fac1aed80084696214ccd7a0730c7f5576c54ceb
waltjohnson <walt@inertialsense.com>
2017-02-13 14:35:07 -0700

    Added doxygen generation files.

---
+++ resources/update_doxygen_docs.bat
@@ -0,0 +1,42 @@
+@echo off
+setlocal enabledelayedexpansion
+
+:: Parameters
+set sdkDir=doc
+set inputfilename=release-notes-template.txt
+
+:: Read Release version, date, and time
+::set /p releaseversion=<version\releaseVersion.txt
+::set /p releasedate=<version\releaseDate_form.txt
+::set /p releasetime=<version\releaseTime_form.txt
+
+cd ..
+
+echo Before Doxygen
+
+:: Update Doxygen documentation
+::doxygen resources\DoxyfileSDK
+( type resources\DoxyfileSDK & echo PROJECT_NUMBER="Release 1.0.30" ) | doxygen -
+::( type resources\DoxyfileSDK & echo PROJECT_NUMBER="Release %releaseversion%,  %releasedate%,  %releasetime%" ) | doxygen -
+
+
+
+:: Zip files
+::powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::CreateFromDirectory('%sdkDir%','%sdkDir%.zip'); }"
+::set cmdstr=CScript zip_folder.vbs %START_PATH%Firmware %START_PATH%%releasedir%\Firmware.zip
+::echo %cmdstr%
+
+
+exit /b
+:: Script End
+
+
+
+:GetRelease <filename>
+set rtn=-1
+for /F "tokens=2 delims=: " %%b in ('findstr /b "Release " %1') do (
+   set rtn=%%b
+   exit /b
+)
+set rtn=%rtn:"=%
+exit /b
