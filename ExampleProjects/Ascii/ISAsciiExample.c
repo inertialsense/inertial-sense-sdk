f6a1a51359688182f477893f6370ca3dc929cacf
waltjohnson <walt@inertialsense.com>
2018-04-14 21:17:39 -0600

    Revert ASCII Example project to enable only PINS1 message as default.

--- ExampleProjects/Ascii/ISAsciiExample.c
+++ ExampleProjects/Ascii/ISAsciiExample.c
@@ -64,8 +64,7 @@ int main(int argc, char* argv[])
 	// please see the user manual for additional updates and notes
 
 	// Get PINS1 @ 10Hz on the connected serial port, leave all other broadcasts the same
-// 	const char* asciiMessage = "ASCB,0,,,100,,,,,,,";
-	const char* asciiMessage = "ASCB,0,500,500,500,500,500,600,600,600,600,600";
+	const char* asciiMessage = "ASCB,0,,,100,,,,,,,";
 
 	// Get PIMU @ 50Hz, GPGGA @ 5Hz, both serial ports, set all other periods to 0
 	// const char* asciiMessage = "ASCB,3,20,0,0,0,0,0,100,0,0,0";
