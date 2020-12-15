6f26dfc59e81de1c2ae4e45b152fa7db09a1b928
waltjohnson <walt@inertialsense.com>
2020-12-15 14:40:35 -0700

    Update Doxygen

--- docs/html/search/classes_9.js
+++ docs/html/search/classes_9.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['nocase_5fcompare_3274',['nocase_compare',['../structs_case_insensitive_compare_1_1nocase__compare.html',1,'sCaseInsensitiveCompare']]]
+  ['nocase_5fcompare',['nocase_compare',['../structs_case_insensitive_compare_1_1nocase__compare.html',1,'sCaseInsensitiveCompare']]]
 ];
