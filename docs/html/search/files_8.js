44467e4adb34e5137510813743323acd0b530863
waltjohnson <walt@inertialsense.com>
2021-04-02 09:53:45 -0600

    Updated doxygen docs

--- docs/html/search/files_8.js
+++ docs/html/search/files_8.js
@@ -1,13 +1,15 @@
 var searchData=
 [
-  ['test_5fcom_5fmanager_2ecpp_3937',['test_com_manager.cpp',['../test__com__manager_8cpp.html',1,'']]],
-  ['test_5fring_5fbuffer_2ecpp_3938',['test_ring_buffer.cpp',['../test__ring__buffer_8cpp.html',1,'']]],
-  ['time_5fconversion_2ec_3939',['time_conversion.c',['../time__conversion_8c.html',1,'']]],
-  ['time_5fconversion_2eh_3940',['time_conversion.h',['../time__conversion_8h.html',1,'']]],
-  ['tinystr_2ecpp_3941',['tinystr.cpp',['../tinystr_8cpp.html',1,'']]],
-  ['tinystr_2eh_3942',['tinystr.h',['../tinystr_8h.html',1,'']]],
-  ['tinyxml_2ecpp_3943',['tinyxml.cpp',['../tinyxml_8cpp.html',1,'']]],
-  ['tinyxml_2eh_3944',['tinyxml.h',['../tinyxml_8h.html',1,'']]],
-  ['tinyxmlerror_2ecpp_3945',['tinyxmlerror.cpp',['../tinyxmlerror_8cpp.html',1,'']]],
-  ['tinyxmlparser_2ecpp_3946',['tinyxmlparser.cpp',['../tinyxmlparser_8cpp.html',1,'']]]
+  ['test_5fcom_5fmanager_2ecpp_3971',['test_com_manager.cpp',['../test__com__manager_8cpp.html',1,'']]],
+  ['test_5fcom_5fmanager_5f2_2ecpp_3972',['test_com_manager_2.cpp',['../test__com__manager__2_8cpp.html',1,'']]],
+  ['test_5fmath_2ecpp_3973',['test_math.cpp',['../test__math_8cpp.html',1,'']]],
+  ['test_5fring_5fbuffer_2ecpp_3974',['test_ring_buffer.cpp',['../test__ring__buffer_8cpp.html',1,'']]],
+  ['time_5fconversion_2ec_3975',['time_conversion.c',['../time__conversion_8c.html',1,'']]],
+  ['time_5fconversion_2eh_3976',['time_conversion.h',['../time__conversion_8h.html',1,'']]],
+  ['tinystr_2ecpp_3977',['tinystr.cpp',['../tinystr_8cpp.html',1,'']]],
+  ['tinystr_2eh_3978',['tinystr.h',['../tinystr_8h.html',1,'']]],
+  ['tinyxml_2ecpp_3979',['tinyxml.cpp',['../tinyxml_8cpp.html',1,'']]],
+  ['tinyxml_2eh_3980',['tinyxml.h',['../tinyxml_8h.html',1,'']]],
+  ['tinyxmlerror_2ecpp_3981',['tinyxmlerror.cpp',['../tinyxmlerror_8cpp.html',1,'']]],
+  ['tinyxmlparser_2ecpp_3982',['tinyxmlparser.cpp',['../tinyxmlparser_8cpp.html',1,'']]]
 ];
--- docs/html/search/related_2.js
+++ docs/html/search/related_2.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['tixmlattributeset_6351',['TiXmlAttributeSet',['../class_ti_xml_attribute.html#a35a7b7f89f708527677d5078d41ce0bf',1,'TiXmlAttribute']]],
-  ['tixmldocument_6352',['TiXmlDocument',['../class_ti_xml_base.html#a173617f6dfe902cf484ce5552b950475',1,'TiXmlBase::TiXmlDocument()'],['../class_ti_xml_node.html#a173617f6dfe902cf484ce5552b950475',1,'TiXmlNode::TiXmlDocument()'],['../class_ti_xml_parsing_data.html#a173617f6dfe902cf484ce5552b950475',1,'TiXmlParsingData::TiXmlDocument()']]],
-  ['tixmlelement_6353',['TiXmlElement',['../class_ti_xml_base.html#ab6592e32cb9132be517cc12a70564c4b',1,'TiXmlBase::TiXmlElement()'],['../class_ti_xml_node.html#ab6592e32cb9132be517cc12a70564c4b',1,'TiXmlNode::TiXmlElement()'],['../class_ti_xml_text.html#ab6592e32cb9132be517cc12a70564c4b',1,'TiXmlText::TiXmlElement()']]],
-  ['tixmlnode_6354',['TiXmlNode',['../class_ti_xml_base.html#a218872a0d985ae30e78c55adc4bdb196',1,'TiXmlBase']]]
+  ['tixmlattributeset_6403',['TiXmlAttributeSet',['../class_ti_xml_attribute.html#a35a7b7f89f708527677d5078d41ce0bf',1,'TiXmlAttribute']]],
+  ['tixmldocument_6404',['TiXmlDocument',['../class_ti_xml_base.html#a173617f6dfe902cf484ce5552b950475',1,'TiXmlBase::TiXmlDocument()'],['../class_ti_xml_node.html#a173617f6dfe902cf484ce5552b950475',1,'TiXmlNode::TiXmlDocument()'],['../class_ti_xml_parsing_data.html#a173617f6dfe902cf484ce5552b950475',1,'TiXmlParsingData::TiXmlDocument()']]],
+  ['tixmlelement_6405',['TiXmlElement',['../class_ti_xml_base.html#ab6592e32cb9132be517cc12a70564c4b',1,'TiXmlBase::TiXmlElement()'],['../class_ti_xml_node.html#ab6592e32cb9132be517cc12a70564c4b',1,'TiXmlNode::TiXmlElement()'],['../class_ti_xml_text.html#ab6592e32cb9132be517cc12a70564c4b',1,'TiXmlText::TiXmlElement()']]],
+  ['tixmlnode_6406',['TiXmlNode',['../class_ti_xml_base.html#a218872a0d985ae30e78c55adc4bdb196',1,'TiXmlBase']]]
 ];
