fffd360e52c9bbf6c616020f17c818d2223c8cfe
waltjohnson <walt@inertialsense.com>
2021-06-16 17:05:41 -0600

    Update doxygen docs

--- docs/html/search/files_0.js
+++ docs/html/search/files_0.js
@@ -1,10 +1,10 @@
 var searchData=
 [
-  ['cltool_2ecpp_3886',['cltool.cpp',['../cltool_8cpp.html',1,'']]],
-  ['cltool_2eh_3887',['cltool.h',['../cltool_8h.html',1,'']]],
-  ['cltool_5fmain_2ecpp_3888',['cltool_main.cpp',['../cltool__main_8cpp.html',1,'']]],
-  ['com_5fmanager_2ec_3889',['com_manager.c',['../com__manager_8c.html',1,'']]],
-  ['com_5fmanager_2eh_3890',['com_manager.h',['../com__manager_8h.html',1,'']]],
-  ['convert_5fins_2ecpp_3891',['convert_ins.cpp',['../convert__ins_8cpp.html',1,'']]],
-  ['convert_5fins_2eh_3892',['convert_ins.h',['../convert__ins_8h.html',1,'']]]
+  ['cltool_2ecpp_3931',['cltool.cpp',['../cltool_8cpp.html',1,'']]],
+  ['cltool_2eh_3932',['cltool.h',['../cltool_8h.html',1,'']]],
+  ['cltool_5fmain_2ecpp_3933',['cltool_main.cpp',['../cltool__main_8cpp.html',1,'']]],
+  ['com_5fmanager_2ec_3934',['com_manager.c',['../com__manager_8c.html',1,'']]],
+  ['com_5fmanager_2eh_3935',['com_manager.h',['../com__manager_8h.html',1,'']]],
+  ['convert_5fins_2ecpp_3936',['convert_ins.cpp',['../convert__ins_8cpp.html',1,'']]],
+  ['convert_5fins_2eh_3937',['convert_ins.h',['../convert__ins_8h.html',1,'']]]
 ];
--- docs/html/search/files_6.js
+++ docs/html/search/files_6.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['readme_2emd_3962',['README.md',['../_r_e_a_d_m_e_8md.html',1,'']]],
-  ['ring_5fbuffer_2ec_3963',['ring_buffer.c',['../ring__buffer_8c.html',1,'']]],
-  ['ring_5fbuffer_2eh_3964',['ring_buffer.h',['../ring__buffer_8h.html',1,'']]]
+  ['readme_2emd_4009',['README.md',['../_r_e_a_d_m_e_8md.html',1,'']]],
+  ['ring_5fbuffer_2ec_4010',['ring_buffer.c',['../ring__buffer_8c.html',1,'']]],
+  ['ring_5fbuffer_2eh_4011',['ring_buffer.h',['../ring__buffer_8h.html',1,'']]]
 ];
