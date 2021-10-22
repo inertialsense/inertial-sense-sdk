b997ba813787b2d7a66fdd80fb34bac111ef3027
waltjohnson <walt@inertialsense.com>
2021-10-21 21:43:26 -0600

    Added magnitude to plots

--- python/logInspector/logPlotter.py
+++ python/logInspector/logPlotter.py
@@ -1426,12 +1426,13 @@ class logPlot:
 
     def sensorCompGen(self, fig, name, useTime=False):
         fig.suptitle('Sensor Comp ' + name + ' - ' + os.path.basename(os.path.normpath(self.log.directory)))
-        ax = fig.subplots(3, 2, sharex=True)
+        ax = fig.subplots(4, 2, sharex=True)
 
         for i in range(2):
             ax[0, i].set_title('X %s %d' % (name, i))
             ax[1, i].set_title('Y %s %d' % (name, i))
             ax[2, i].set_title('Z %s %d' % (name, i))
+            ax[3, i].set_title('Magnitude %s %d' % (name, i))
             for d in range(3):
                 if useTime:
                     ax[d,i].set_xlabel("Time (s)")
@@ -1452,10 +1453,16 @@ class logPlot:
                 sensor = mpu[:,i]['lpfLsb'][name]
                 if name=='pqr':
                     sensor *= RAD2DEG
+
+                if name=='acc' and sensor[:,2][0] > 4:
+                    sensor[:,2] -= 19.6
+
                 # ax[0,i].plot(temp, sensor[:,0], label=self.log.serials[d] if i==0 else None )
                 ax[0,i].plot(temp, sensor[:,0], label=self.log.serials[d] )
                 ax[1,i].plot(temp, sensor[:,1])
                 ax[2,i].plot(temp, sensor[:,2])
+                if name=='acc':
+                    ax[3,i].plot(temp, np.linalg.norm(sensor, axis=1))
 
         # Show serial numbers
         ax[0,0].legend(ncol=2)
