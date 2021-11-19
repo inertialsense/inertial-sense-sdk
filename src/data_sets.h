8d5b102e922b3f54334768095744ac01da8311eb
Dave Cutting <davidcutting42@gmail.com>
2021-11-19 11:55:24 -0700

    Reset number of serial ports to 6

--- src/data_sets.h
+++ src/data_sets.h
@@ -1344,7 +1344,7 @@ typedef struct PACKED
 
 #define NUM_COM_PORTS       4	// Number of communication ports.  (Ser0, Ser1, Ser2, and USB).
 #ifndef NUM_SERIAL_PORTS
-#define NUM_SERIAL_PORTS	4
+#define NUM_SERIAL_PORTS	6
 #endif
 
 /** Realtime Message Controller (used in rmc_t). 
