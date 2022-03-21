81576528d5749d43d300b735d65ef26995bbd3b0
Dave Cutting <dave.cutting@inertialsense.com>
2022-03-20 19:05:49 -0600

    Fix includes

--- src/ISBootloaderDfu.h
+++ src/ISBootloaderDfu.h
@@ -30,7 +30,6 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 #include <stdbool.h>
 
 #include "ihex.h"
-#include "ISBootloaderLog.h"
 #include "ISBootloaderTypes.h"
 #include "ISBootloaderCommon.h"
 #include "libusb/libusb.h"
--- src/ISBootloaderSamba.h
+++ src/ISBootloaderSamba.h
@@ -33,7 +33,6 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 #include <stdbool.h>
 
 #include "ihex.h"
-#include "ISBootloaderLog.h"
 #include "ISBootloaderTypes.h"
 #include "ISBootloaderCommon.h"
 
