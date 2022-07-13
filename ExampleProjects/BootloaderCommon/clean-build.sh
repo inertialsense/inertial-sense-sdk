f4e0be7a49f272f9530a273018a77fdc438a00ed
Dave Cutting <davidcutting42@gmail.com>
2022-07-13 10:31:31 -0600

    Fix bootloader example project build

--- ExampleProjects/BootloaderCommon/clean-build.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-rm -rf build && mkdir build && cd build && cmake -DCMAKE_BUILD_TYPE=Debug .. && make -j $(grep ^processor /proc/cpuinfo  | wc -l) && cd .. && chown -R 1000 build
