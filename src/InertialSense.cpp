1b8b136534f602dda398e6f6e6055c84bdd326e7
Filip Matzner <floop@floop.cz>
2022-08-13 21:36:49 +0200

    [InertialSense] Reconnect after failure with delay

--- src/InertialSense.cpp
+++ src/InertialSense.cpp
@@ -13,6 +13,9 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 #include "protocol_nmea.h"
 #include "InertialSense.h"
 
+#include <chrono>
+#include <thread>
+
 using namespace std;
 
 typedef struct
@@ -515,6 +518,7 @@ bool InertialSense::UpdateClient()
 	{
 		CloseServerConnection();
 		m_clientStreamReconnector = std::async(std::launch::async, [this]() {
+			std::this_thread::sleep_for(std::chrono::milliseconds{IS_SOCKET_DEFAULT_TIMEOUT_MS});
 			return cISClient::OpenConnectionToServer(m_clientConnectionString, &m_forwardGpgga);
 		});
 		return false;
