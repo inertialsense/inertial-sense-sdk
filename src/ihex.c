8fc8be4718fb78bf0ab9f4f828255f8cd3273c40
waltjohnson <walt@inertialsense.com>
2023-01-13 12:43:58 -0700

    Fix merge conflict

--- src/ihex.c
+++ src/ihex.c
@@ -79,12 +79,7 @@ static int ihex_load_section(FILE** ihex_file, ihex_image_section_t* section)
         line[0] = '\0';
 
         last_line = ftell(*ihex_file);
-<<<<<<< HEAD
-        char* result = fgets(line, 512, *ihex_file);
-        (void*)result;
-=======
         if (fgets(line, 512, *ihex_file)){}
->>>>>>> origin/development
 
         // Turn end of line characters into cstring terminators
         if (line[strlen(line) - 1] == '\n') line[strlen(line) - 1] = '\0';
