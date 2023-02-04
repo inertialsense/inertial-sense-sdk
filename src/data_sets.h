20800f33a21d3c7acf5e74155c510cfa47f502f7
waltjohnson <walt@inertialsense.com>
2023-02-03 22:30:54 -0700

    WIP

--- src/data_sets.h
+++ src/data_sets.h
@@ -2311,6 +2311,8 @@ enum eIoConfig
 	IO_CONFIG_GPS_TYPE_UBX_F9P					= (int)1,
 	/** GPS type - NMEA */
 	IO_CONFIG_GPS_TYPE_NMEA						= (int)2,
+	/** GPS type - InertialSense GPX */
+	IO_CONFIG_GPS_TYPE_GPX						= (int)3,
 	/** GPS type - last type */
     IO_CONFIG_GPS_TYPE_LAST						= IO_CONFIG_GPS_TYPE_NMEA,		// Set to last type
 
@@ -2352,11 +2354,11 @@ enum ePlatformConfig
 	PLATFORM_CFG_TYPE_IG1_0_G2                  = (int)13,      // PCB IG-1.0.  GPS1 timepulse on G8
 	PLATFORM_CFG_TYPE_IG1_G1                    = (int)14,      // PCB IG-1.1 and later.  GPS1 timepulse on GPS1_PPS TIMESYNC (pin 20)
 	PLATFORM_CFG_TYPE_IG1_G2                    = (int)15,
-	PLATFORM_CFG_TYPE_LAMBDA_G1                 = (int)16,		// Enable UBX output on Lambda for testbed
-	PLATFORM_CFG_TYPE_LAMBDA_G2                 = (int)17,		// "
-	PLATFORM_CFG_TYPE_TESTBED_G1_W_LAMBDA       = (int)18,		// Enable UBX input from Lambda
-	PLATFORM_CFG_TYPE_TESTBED_G2_W_LAMBDA       = (int)19,		// "
-	PLATFORM_CFG_TYPE_COUNT                     = (int)20,
+	PLATFORM_CFG_TYPE_IG2                       = (int)16,		// IG-2 w/ IMX-5 and GPX-1
+	PLATFORM_CFG_TYPE_LAMBDA_G1                 = (int)50,		// Enable UBX output on Lambda for testbed
+	PLATFORM_CFG_TYPE_LAMBDA_G2                 = (int)51,		// "
+	PLATFORM_CFG_TYPE_TESTBED_G1_W_LAMBDA       = (int)52,		// Enable UBX input from Lambda
+	PLATFORM_CFG_TYPE_TESTBED_G2_W_LAMBDA       = (int)53,		// "
 
 	// Presets
 	PLATFORM_CFG_PRESET_MASK                    = (int)0x0000FF00,
