547cf7de7a92d688e74409b491cc18a9a309fe52
Walt Johnson <walt@inertialsense.com>
2023-03-14 09:28:38 -0600

    Simplify system command interface (#372)

--- src/InertialSense.h
+++ src/InertialSense.h
@@ -261,7 +261,7 @@ public:
 	* @param pHandle the pHandle to set sysCmd for
 	* @param sysCmd new device configuration
 	*/
-	void SetSysCmd(const system_command_t& sysCmd, int pHandle = 0);
+	void SetSysCmd(const uint32_t command, int pHandle = -1);
 
 	/**
 	* Get the flash config, returns the latest flash config read from the uINS flash memory
