42874c17260f366b37d41a49c6682b5f8ff61213
Andrew Priddis <andrew.priddis@inertialsense.com>
2023-04-19 10:01:38 -0600

    Batch file install yaml-cpp

--- scripts/install_yaml-cpp.bat
+++ scripts/install_yaml-cpp.bat
@@ -4,26 +4,37 @@ set YAMLCPP_VERSION=0.7.0
 set YAMLCPP_FOLDER=yaml-cpp-%YAMLCPP_VERSION%
 set YAMLCPP_ZIP=%YAMLCPP_FOLDER%.zip
 
-set YAMLCPP_URL=https://github.com/jbeder/yaml-cpp/archive/%YAMLCPP_VERSION%.zip
+set YAMLCPP_URL=https://github.com/jbeder/yaml-cpp/archive/refs/tags/%YAMLCPP_FOLDER%.zip
 
 set YAMLCPP_BUILD_FOLDER=%YAMLCPP_FOLDER%\build
 set YAMLCPP_LIB_FOLDER=%YAMLCPP_BUILD_FOLDER%\Release
 
 echo Downloading yaml-cpp %YAMLCPP_VERSION%...
-curl -L -o %YAMLCPP_ZIP% %YAMLCPP_URL%
+echo from %YAMLCPP_URL%
+powershell -command "& { Invoke-WebRequest -Uri %YAMLCPP_URL% -OutFile %YAMLCPP_ZIP% }"
 
 echo Extracting yaml-cpp %YAMLCPP_VERSION%...
-7z x %YAMLCPP_ZIP%
+powershell -command "& { Expand-Archive -Path %YAMLCPP_ZIP%}"
 
 echo Building yaml-cpp %YAMLCPP_VERSION%...
-mkdir %YAMLCPP_BUILD_FOLDER%
-cd %YAMLCPP_BUILD_FOLDER%
-cmake -G "Visual Studio 16 2019" -DCMAKE_BUILD_TYPE=Release .. > nul
-cmake --build . --config Release > nul
+
+echo cd %YAMLCPP_FOLDER%
+cd %YAMLCPP_FOLDER%
+echo cd yaml-cpp-%YAMLCPP_FOLDER%
+cd yaml-cpp-%YAMLCPP_FOLDER%
+mkdir build
+cd build
+cmake ..
+cmake --build . --config Release
 
 echo Installing yaml-cpp %YAMLCPP_VERSION%...
-xcopy /y /q /i %YAMLCPP_LIB_FOLDER%\libyaml-cpp.lib C:\Windows\System32
-xcopy /y /q /i %YAMLCPP_FOLDER%\include C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.29.30133\include
+cd Release
+echo %cd%
+
+xcopy /y /q /i yaml-cpp.lib C:\Windows\System32
+cd ../..
+echo Current Directory: %cd%
+xcopy "include" "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.29.30133\include" /E /H /C /I
 
 echo Cleaning up...
 cd ..\..
