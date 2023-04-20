ff48bfa538ae42efd67a4a766fdf12c44d1a4903
waltjohnson <walt@inertialsense.com>
2023-04-20 00:58:54 -0600

    Fix dependency

--- scripts/install_sdk_dependencies.sh
+++ scripts/install_sdk_dependencies.sh
@@ -7,7 +7,7 @@ echo_blue "==============================================="
 echo_blue " Install Dependencies: SDK                     "
 echo_blue "==============================================="
 
-sudo apt install -y cmake unzip zip libyaml-cpp-dev 
+sudo apt install -y cmake unzip zip libyaml-cpp-dev libudev-dev
 
 source ~/.bashrc
 
