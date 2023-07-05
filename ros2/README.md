e7c610ef9c37dbe0efd76a1ef511d4d37a8b8f02
Achmad Fathoni <fathoni.id@gmail.com>
2023-07-05 14:16:24 +0700

    Clean up launch file

--- ros2/README.md
+++ ros2/README.md
@@ -7,5 +7,5 @@ git clone https://github.com/AchmadFathoni/inertial-sense-sdk --recurse-submodul
 cd ..
 colcon build --paths src/inertial-sense-sdk/ros2 --symlink-install
 source install/setup.bash
-ros2 launch inertial_sense_ros example.launch.py
+ros2 launch inertial_sense_ros example_params.launch.py
 ```
--- ros2/launch/example_params.launch.py
+++ ros2/launch/example_params.launch.py
@@ -1,17 +1,13 @@
-# <launch>
-	# <node name="inertial_sense_node" pkg="inertial_sense_ros" exec="inertial_sense_node" 
-		# output="screen" args="$(find inertial_sense_ros)/launch/example_params.yaml"/>
-# </launch>
-import launch
-import launch_ros.actions
+from launch import LaunchDescription
+from launch_ros.actions import Node
 
 from ament_index_python.packages import get_package_share_path
 
 def generate_launch_description():
     package = 'inertial_sense_ros'
     yaml_path = get_package_share_path(package)/'launch/params.yaml'
-    return launch.LaunchDescription([
-        launch_ros.actions.Node(
+    return LaunchDescription([
+        Node(
             package = package,
             executable = 'inertial_sense_node',
             arguments = [str(yaml_path)],
