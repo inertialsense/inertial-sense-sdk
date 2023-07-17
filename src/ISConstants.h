57f86cf71696f440460f8fc1d11d35bbf2912170
Walt Johnson <walt@inertialsense.com>
2023-07-17 16:26:05 -0600

    Dev v2 zephyr merge (#412)
    * Update includes
    
    * Prep SDK for Zephyr
    
    * Fix includes
    
    * include fix
    
    * Fix Zephyr snprintf
    
    * Revert zephyr refactor
    
    * Add critical section macros
    
    * Move time functions into ISEarth
    
    * Revert debug commented out code
    
    * Revert changes
    
    ---------
    
    Co-authored-by: Dave Cutting <davidcutting42@gmail.com>

--- src/ISConstants.h
+++ src/ISConstants.h
@@ -143,7 +143,13 @@ extern void vPortFree(void* pv);
 
 #endif 
 
-#if PLATFORM_IS_EMBEDDED
+#if __ZEPHYR__
+#include <zephyr/irq.h>
+#define BEGIN_CRITICAL_SECTION irq_lock();
+#define END_CRITICAL_SECTION irq_unlock(0);
+// #define SNPRINTF snprintfcb
+#define SNPRINTF snprintf_
+#elif PLATFORM_IS_EMBEDDED
 #include "printf.h"		// Use embedded-safe SNPRINTF
 #define SNPRINTF snprintf_
 #define VSNPRINTF vsnprintf_
@@ -352,8 +358,10 @@ extern void vPortFree(void* pv);
 #define OFFSET_OF_MEMBER_INDEX_SUBMEMBER(type, member, i, submember) (offsetof(type, member[0].submember) + (i) * MEMBER_ITEM_SIZE(type, member))
 #endif
 
+#ifndef __ZEPHYR__
 #ifndef STRINGIFY
 #define STRINGIFY(x) #x
+#endif
 #endif
 
 #ifndef M_PI
@@ -414,7 +422,11 @@ extern void vPortFree(void* pv);
 #define PRE_PROC_COMBINE(X, Y) X##Y
 #ifndef STATIC_ASSERT
 // #define STATIC_ASSERT_MSG(exp, msg) typedef char PRE_PROC_COMBINE(msg, __LINE__)[(exp) ? 1 : -1]
+#ifdef __cplusplus
 #define STATIC_ASSERT(exp) static_assert(exp, #exp)
+#else
+#define STATIC_ASSERT(exp) _Static_assert(exp, #exp)
+#endif
 #endif
 #ifndef OVERRIDE
 #define OVERRIDE
--- src/ISEarth.c
+++ src/ISEarth.c
@@ -831,3 +831,99 @@ void gndSpeedToVelEcef(const float gndSpeed, const float hdg, const float vertVe
 
     mul_Mat3x3_Vec3x1(velEcef, Rn2e, velNed);
 }
+
+#if !defined(RTK_EMBEDDED)
+static const double gpst0[]={1980, 1, 6,  0, 0, 0}; /* gps time reference */
+static const double gst0 []={1999, 8, 22, 0, 0, 0}; /* galileo system time reference */
+static const double bdt0 []={2006, 1, 1,  0, 0, 0}; /* beidou time reference */
+#define MAXLEAPS    64                  /* max number of leap seconds table */
+static double leaps[MAXLEAPS + 1][7] = { /* leap seconds (y,m,d,h,m,s,utc-gpst) */
+    {2017,1,1,0,0,0,-18},
+    {2015,7,1,0,0,0,-17},
+    {2012,7,1,0,0,0,-16},
+    {2009,1,1,0,0,0,-15},
+    {2006,1,1,0,0,0,-14},
+    {1999,1,1,0,0,0,-13},
+    {1997,7,1,0,0,0,-12},
+    {1996,1,1,0,0,0,-11},
+    {1994,7,1,0,0,0,-10},
+    {1993,7,1,0,0,0, -9},
+    {1992,7,1,0,0,0, -8},
+    {1991,1,1,0,0,0, -7},
+    {1990,1,1,0,0,0, -6},
+    {1988,1,1,0,0,0, -5},
+    {1985,7,1,0,0,0, -4},
+    {1983,7,1,0,0,0, -3},
+    {1982,7,1,0,0,0, -2},
+    {1981,7,1,0,0,0, -1},
+    {0}
+};
+
+gtime_t epoch2time(const double *ep)
+{
+    const int doy[] = { 1,32,60,91,121,152,182,213,244,274,305,335 };
+    gtime_t time = { 0 };
+    int days, sec, year = (int)ep[0], mon = (int)ep[1], day = (int)ep[2];
+
+    if (year < 1970 || 2099 < year || mon < 1 || 12 < mon)
+        return time;
+
+    /* leap year if year%4==0 in 1901-2099 */
+    days = (year - 1970) * 365 + (year - 1969) / 4 + doy[mon - 1] + day - 2 + (year % 4 == 0 && mon >= 3 ? 1 : 0);
+    sec = (int)floor(ep[5]);
+    time.time = (time_t)days * 86400 + (int)ep[3] * 3600 + (int)ep[4] * 60 + sec;
+    time.sec = ep[5] - sec;
+    return time;
+}
+
+gtime_t timeadd(gtime_t t, double sec)
+{
+    double tt;
+    t.sec += sec; 
+    tt = floor(t.sec); 
+    t.time += (time_t)tt; 
+    t.sec -= tt;
+    return t;
+}
+
+double timediff(gtime_t t1, gtime_t t2)
+{
+    return ((double)(t1.time - t2.time)) + (t1.sec - t2.sec);
+}
+
+gtime_t utc2gpst(gtime_t t)
+{
+    int i;
+
+    for (i = 0; leaps[i][0] > 0; i++) {
+        if (timediff(t, epoch2time(leaps[i])) >= 0.0) {
+            return timeadd(t, -leaps[i][6]);
+        }
+    }
+    return t;
+}
+
+gtime_t gpst2time(int week, double sec)
+{
+    gtime_t t = epoch2time(gpst0);
+
+    if (sec < -1E9 || 1E9 < sec) 
+        sec = 0.0;
+    t.time += (time_t)86400 * 7 * week + (time_t)sec;
+    t.sec = sec - (time_t)sec;
+    return t;
+}
+
+double time2gpst(gtime_t t, int *week)
+{
+    gtime_t t0 = epoch2time(gpst0);
+    time_t sec = t.time - t0.time;
+    time_t w = (time_t)(sec / (86400 * 7));
+
+    if (week) 
+        *week = (int)w;
+    return (double)(sec - (double)w * 86400 * 7) + t.sec;
+}
+
+#endif // #if defined(RTK_EMBEDDED)
+
