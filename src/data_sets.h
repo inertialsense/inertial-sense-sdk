01ca9ff9294ac940ced2c1b1c003624ed5761c2d
Alexander Bogdanov <alex.bogdanov@inertialsense.com>
2023-08-30 17:04:40 -0700

    removed unused constant

--- src/data_sets.h
+++ src/data_sets.h
@@ -3096,7 +3096,6 @@ PUSH_PACK_1
 #define MAXSAT      (NSATGPS+NSATGLO+NSATGAL+NSATQZS+NSATCMP+NSATIRN+NSATSBS+NSATLEO)
 #define NEXOBS      0           /* number of extended obs codes */
 #define MAXOBS      19          /* max number of obs (satellites) in an epoch */ // Note: total number of observed satellites for rover + base is (2 * MAXOBS)
-#define MAXHOLDAMBOBS _MIN(30, MAXSAT*NFREQ)   /* max number of observations in holdamb(). This is needed only due to memory constraints */
 #define MAXERRMSG   0           /* max length of error/warning message */
 #define MAXANT      64          /* max length of station name/antenna type */
 #define MAXPREOBS  _MAX(40, MAXOBS)  // Note: total rover + base is (2 * MAXPREOBS)
