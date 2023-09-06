e31cc4c1bcb51ed86a131a436e3780f7481f047a
tmcd249 <tmcd249@msn.com>
2023-09-06 17:34:47 -0600

    V2 log down grade (#440)
    * add macro to turn on conver
    
    * match rtkv2 convert
    
    * sdk changes flash cnfig
    
    * remove dead code
    
    * changes var name to describe purpose

--- src/ISConstants.h
+++ src/ISConstants.h
@@ -30,6 +30,9 @@ extern "C" {
 
 #define ECEF2LLA_METHOD 5  // Method to compute LLA from ECEF position (0 through 5)
 
+#define CONVERT_RAW_GPS_V2_TO_V1    0      // Allow conversion of raw GPS format from v1 to v2 used for RTK
+#define FORCE_GPX_DEFAULT_FLASH     0       // forces gpx flash not to be overwriten by IMX's data
+
 #if defined(WIN32) || defined(__WIN32__) || defined(_WIN32)
 
 #define PLATFORM_IS_WINDOWS 1
