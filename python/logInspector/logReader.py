Fixed RMS computation for the GPS compassing case

python/logInspector/logReader.py
@@ -325,10 +325,11 @@ class Log:
         mount_bias_output = dict()
         for n, dev in enumerate(uINS_device_idx):
             mount_bias = np.mean(self.att_error[n, :, :], axis=0)
-            if self.compassing:
+            #if self.compassing:
                 # When in compassing, assume all units are sharing the same GPS antennas and should therefore have
                 # no mounting bias in heading
-                mount_bias[2] = 0
+                # NOTE 2: The units can have a common mounting bias relative to antenna offsets. So we should keep non-zero mount_bias[2]
+            #    mount_bias[2] = 0
             self.mount_bias_euler[dev, :] = mount_bias  # quat2eulerArray(qexp(mount_bias))
             self.mount_bias_quat[dev,:] = euler2quat(self.mount_bias_euler[dev, :])
             self.using_mounting_bias = True