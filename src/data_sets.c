92e8f9d011334b7c4468ea4d51ee21a8235c0400
waltjohnson <walt@inertialsense.com>
2024-01-18 16:58:03 -0700

    Addressed some linter warnings.

--- src/data_sets.c
+++ src/data_sets.c
@@ -80,10 +80,9 @@ void flipEndianess32(uint8_t* data, int dataLength)
 	
 	uint32_t* dataPtr = (void*)data;
 	uint32_t* dataPtrEnd = (void*)(data + dataLength);
-	uint32_t tmp;
 	while (dataPtr < dataPtrEnd)
 	{
-		tmp = *dataPtr;
+		uint32_t tmp = *dataPtr;
 		*dataPtr++ = SWAP32(tmp);
 	}
 }
@@ -91,13 +90,11 @@ void flipEndianess32(uint8_t* data, int dataLength)
 void flipDoubles(uint8_t* data, int dataLength, int offset, uint16_t* offsets, uint16_t offsetsLength)
 {
 	uint16_t* doubleOffsetsEnd = offsets + offsetsLength;
-	int offsetToDouble;
 	int maxDoubleOffset = dataLength - 8;
-    int isDouble;
 	while (offsets < doubleOffsetsEnd)
 	{
-        offsetToDouble = (*offsets++);
-        isDouble = ((offsetToDouble & 0x8000) == 0);
+        int offsetToDouble = (*offsets++);
+        int isDouble = ((offsetToDouble & 0x8000) == 0);
         offsetToDouble = (offsetToDouble & 0x7FFF) - offset;
 		if (offsetToDouble >= 0 && offsetToDouble <= maxDoubleOffset)
 		{
@@ -117,15 +114,12 @@ void flipDoubles(uint8_t* data, int dataLength, int offset, uint16_t* offsets, u
 void flipStrings(uint8_t* data, int dataLength, int offset, uint16_t* offsets, uint16_t offsetsLength)
 {
 	uint16_t* stringOffsetsEnd = offsets + offsetsLength;
-	int offsetToString;
-	int lengthOfString;
-	int maxStringOffset;
 
 	while (offsets < stringOffsetsEnd)
 	{
-		offsetToString = (*offsets++) - offset;
-		lengthOfString = (*offsets++);
-		maxStringOffset = dataLength - lengthOfString;
+		int offsetToString = (*offsets++) - offset;
+		int lengthOfString = (*offsets++);
+		int maxStringOffset = dataLength - lengthOfString;
 		if (offsetToString >= 0 && offsetToString <= maxStringOffset)
 		{
 			flipEndianess32(data + offsetToString, lengthOfString);
@@ -395,8 +389,8 @@ uint16_t* getDoubleOffsets(eDataIDs dataId, uint16_t* offsetsLength)
 
 	if (dataId < DID_COUNT)
 	{
-        uint16_t* offsets;
-        if ((offsets = s_doubleOffsets[dataId]))
+        uint16_t* offsets = s_doubleOffsets[dataId];
+        if (offsets)
         {
             *offsetsLength = (*offsets++);
             return offsets;
@@ -572,8 +566,8 @@ uint16_t* getStringOffsetsLengths(eDataIDs dataId, uint16_t* offsetsLength)
 
     if (dataId < DID_COUNT)
 	{
-        uint16_t* offsets;
-        if ((offsets = s_stringOffsets[dataId]))
+        uint16_t* offsets = s_stringOffsets[dataId];
+        if (offsets)
         {
             *offsetsLength = (*offsets++);
             return offsets;
