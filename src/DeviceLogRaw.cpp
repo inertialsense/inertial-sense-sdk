fba1ac1da8c173ea11fdd0d002c18021ff55dda1
waltjohnson <walt@inertialsense.com>
2024-03-12 01:59:11 -0600

    Display parse errors

--- src/DeviceLogRaw.cpp
+++ src/DeviceLogRaw.cpp
@@ -110,7 +110,7 @@ bool cDeviceLogRaw::SaveData(int dataSize, const uint8_t* dataBuf, cLogStats &gl
 				break;
 
 			case _PTYPE_PARSE_ERROR:
-				if (m_showParseErrors){ printf("Parse error in cDeviceLogRaw::SaveData()!!!\n"); }
+				if (m_showParseErrors){ printf("SaveData() parse errors: %d\n", m_comm.rxErrorCount); }
 				break;
 
 			case _PTYPE_INERTIAL_SENSE_DATA:
@@ -256,7 +256,7 @@ p_data_t* cDeviceLogRaw::ReadDataFromChunk()
 				break;
 
 			case _PTYPE_PARSE_ERROR:
-				if (m_showParseErrors){ printf("Parse error in cDeviceLogRaw::ReadDataFromChunk()!!!\n"); }
+				if (m_showParseErrors){ printf("ReadDataFromChunk() parse errors: %d\n", m_comm.rxErrorCount); }
 				break;
 
 			case _PTYPE_INERTIAL_SENSE_DATA:
