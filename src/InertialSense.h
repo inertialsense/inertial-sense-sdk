0ef02098281435b01a4011efca96f2d5f8523f33
Walt Johnson <walt@inertialsense.com>
2024-03-15 11:31:16 -0600

    Wait until flash config is received before printing.  Remove EVB from cltool (#571)

--- src/InertialSense.h
+++ src/InertialSense.h
@@ -69,7 +69,6 @@ public:
         system_command_t sysCmd;
         nvm_flash_cfg_t flashCfg;
         unsigned int flashCfgUploadTimeMs;		// (ms) non-zero time indicates an upload is in progress and local flashCfg should not be overwritten
-        evb_flash_cfg_t evbFlashCfg;
         sys_params_t sysParams;
         ISFirmwareUpdater *fwUpdater;
         fwUpdate::update_status_e closeStatus;
@@ -320,22 +319,6 @@ public:
     * @return int number bytes sent
     */
     int SetFlashConfig(nvm_flash_cfg_t &flashCfg, int pHandle = 0);
-
-    /**
-    * Get the EVB flash config, returns the latest flash config read from the uINS flash memory
-    * @param flashCfg the flash config value
-    * @param pHandle the port pHandle to get flash config for
-    * @return bool whether the EVB flash config is valid, currently synchronized
-    */
-    bool EvbFlashConfig(evb_flash_cfg_t &evbFlashCfg, int pHandle = 0);
-
-    /**
-    * Set the EVB flash config and update flash config on the EVB-2 flash memory
-    * @param evbFlashCfg the flash config
-    * @param pHandle the pHandle to set flash config for
-    * @return int number bytes sent
-    */
-    int SetEvbFlashConfig(evb_flash_cfg_t &evbFlashCfg, int pHandle = 0);
 
     void ProcessRxData(int pHandle, p_data_t* data);
     void ProcessRxNmea(int pHandle, const uint8_t* msg, int msgSize);
