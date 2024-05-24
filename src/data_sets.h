08994738a71055e548d6b2ad0989fe882cfb8f76
tmcd249 <tmcd249@msn.com>
2024-05-23 19:52:07 -0600

    Update data_sets.h
    fixed error

--- src/data_sets.h
+++ src/data_sets.h
@@ -4676,8 +4676,6 @@ enum DID_EventProtocol
     DID_EventProtocol_RTMC3_rcvr1       = 11,
     DID_EventProtocol_RTMC3_rcvr2       = 12,
     DID_EventProtocol_RTMC3_ext         = 13,
-    DID_EventProtocol_RTMC3_rcvr2       = 12,
-    DID_EventProtocol_RTMC3_ext         = 13,
     DID_EventProtocol_SonyBinProt_rcvr1 = 14,
     DID_EventProtocol_SonyBinProt_rcvr2 = 15,
 };
