28c78adbd38209261774296e57c1d1af920ec3b5
FiriusFoxx <FiriusFoxx@yahoo.com>
2024-05-24 22:01:52 -0600

    Fix General SW Linux Build (#626)
    * Remove hardcoded binary path for python in install_pybind.sh
    * Fix infinite loop in IMX CI-HDW

--- scripts/install_pybind.sh
+++ scripts/install_pybind.sh
@@ -4,7 +4,7 @@ function echo_blue   { echo -e "\033[1;34m$@\033[0m"; }
 pushd "$(dirname "$(realpath $0)")" > /dev/null
 source ../SDK/scripts/build_test_cmake.sh
 
-/usr/bin/python3 -m pip install pytest pybind11
+python3 -m pip install pytest pybind11
 
 source ~/.bashrc
 popd > /dev/null
