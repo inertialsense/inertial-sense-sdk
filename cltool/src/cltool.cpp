ba9ea647e03453c4ae666012a539bb2280788819
tmcd249 <tmcd249@msn.com>
2024-06-05 14:11:39 -0600

    update CLtool help output

--- cltool/src/cltool.cpp
+++ cltool/src/cltool.cpp
@@ -613,7 +613,16 @@ void cltool_outputUsage()
 	cout << "    -uf " << boldOff << "FILEPATH    Update application firmware using .hex file FILEPATH.  Add -baud=115200 for systems w/ baud rate limits." << endlbOn;
 	cout << "    -ub " << boldOff << "FILEPATH    Update bootloader using .bin file FILEPATH if version is old. Must be used along with option -uf." << endlbOn;
 	cout << "    -fb " << boldOff << "            Force bootloader update regardless of the version." << endlbOn;
-	cout << "    -uv " << boldOff << "            Run verification after application firmware update." << endlbOn;
+    cout << "    -uv " << boldOff << "            Run verification after application firmware update." << endlbOn;
+    cout << "    -evf=[t],[po],[pr],[id]" << boldOff << "    Sets which DID_EVENT's can be broadcast for debug purposes." << endlbOn;
+    cout << "         target: t=[0=device, 1=device's GNSS1 port, 2=device's GNSS2 port]," << endlbOn;
+    cout << "         portMask: po=[0x80=currentPort, 0x08=USB port, 0x04=UART2, 0x02=UART1, 0x01=UART)]," << endlbOn;
+    cout << "         priorityMask: pr=[Priority ID's to be enabled. Mask together protocol EV_ID value (0x01 << EV_ID)." << endlbOn;
+    cout << "             See:eEventPriority for protocol EV_ID values]. It is recommended to mask 0xc0" << endlbOn;
+    cout << "             at all times to allow broadcast of critical errors,"  << endlbOn;
+    cout << "         idMask id=[Protocol ID's to be enabled. Mask together protocol EV_ID value (0x01 << EV_ID)." << endlbOn;
+    cout << "             See:eEventProtocol for protocol EV_ID values]. It is recommended to mask (0x01 << EVENT_PROTOCOL_ASCII)" << endlbOn;
+    cout << "             at all times to allow broadcast of critical errors." << endlbOn;
 	cout << "    -sysCmd=[c]" << boldOff << "     Send DID_SYS_CMD c (see eSystemCommand) preceeded by unlock command then exit the program." << endlbOn;
 	cout << "    -factoryReset " << boldOff << "  Reset IMX flash config to factory defaults." << endlbOn;
 	cout << "    -romBootloader " << boldOff << " Reboot into ROM bootloader mode.  Requires power cycle and reloading bootloader and firmware." << endlbOn;
