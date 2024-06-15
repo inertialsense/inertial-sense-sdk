04f826e755d8a79f02c88ec9a107ae033d7ebd01
waltjohnson <walt@inertialsense.com>
2024-06-15 16:21:48 -0600

    Fix ISCommunicationsExample.cpp

--- ExampleProjects/Communications/ISCommunicationsExample.cpp
+++ ExampleProjects/Communications/ISCommunicationsExample.cpp
@@ -17,6 +17,7 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 #include "../../src/ISComm.h"
 #include "../../src/serialPortPlatform.h"
 #include "../../src/ISPose.h"
+#include "../../src/protocol_nmea.h"
 
 static int running = 1;
 
