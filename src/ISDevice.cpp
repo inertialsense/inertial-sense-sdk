def503e4a7a2592ca9cdd6aa0c26af5420a22414
Kyle Mallory <kyle@inertialsense.com>
2024-07-01 12:33:27 -0600

    ISDevice expansion

--- src/ISDevice.cpp
+++ src/ISDevice.cpp
@@ -12,6 +12,8 @@
 #include "ISBFirmwareUpdater.h"
 #include "ISDFUFirmwareUpdater.h"
 
+#include "protocol_nmea.h"
+
 bool ISDevice::fwUpdateInProgress() { return (fwUpdater && !fwUpdater->fwUpdate_isDone()); }
 
 bool ISDevice::fwUpdate() {
@@ -159,4 +161,190 @@ bool ISDevice::queryDeviceInfo() {
             return true;
     }
     return false;
+}
+
+bool ISDevice::open(int baudRate, bool validate) {
+
+    if (serialPortIsOpen(&serialPort))
+        return true;
+
+    int ret = serialPortOpen(&serialPort, serialPort.port, baudRate, false);
+    if (ret != 0)
+        return false;
+
+    if (!validate)
+        return true;
+
+    return validateDevice(5000); // 5 seconds to successfully validate
+}
+
+bool ISDevice::stopBroadcasts(bool allPorts) {
+    comManagerSendRawInstance(&cmInstance, portHandle, (uint8_t*)(allPorts ? NMEA_CMD_STOP_ALL_BROADCASTS_ALL_PORTS : NMEA_CMD_STOP_ALL_BROADCASTS_CUR_PORT), NMEA_CMD_SIZE);
+    return false;
+}
+
+bool ISDevice::validateDevice(uint32_t timeout) {
+    unsigned int startTime = current_timeMs();
+    int queryType = 0;  // we cycle through different types of queries looking for the first response (0 = NMEA, 1 = ISbinary, 2 = ISbootloader, 3 = MCUboot/SMP)
+
+    if (!isOpen())
+        return false;
+
+    // check for Inertial-Sense App by making an NMEA request (which it should respond to)
+    do {
+        if (serialPort.errorCode == ENOENT)
+            return false;
+
+        switch (queryType) {
+            case QUERYTYPE_NMEA:
+                comManagerSendRawInstance(cmInstance, portHandle, (uint8_t *) NMEA_CMD_QUERY_DEVICE_INFO, NMEA_CMD_SIZE);
+                break;
+            case QUERYTYPE_ISB:
+                comManagerSendRawInstance(cmInstance, portHandle, (uint8_t *) DID_DEV_INFO, sizeof(dev_info_t));
+                break;
+            case QUERYTYPE_IBbootloader:
+                queryDeviceInfo();
+                break;
+            case QUERYTYPE_mcuBoot:
+                break;
+
+        }
+        // there was some other janky issue with the requested port; even though the device technically exists, its in a bad state. Let's just drop it now.
+        if (serialPort.errorCode != 0)
+            return false;
+
+        SLEEP_MS(100);
+        stepComms();
+
+        if ((current_timeMs() - startTime) > timeout)
+            return false;
+
+        queryType = static_cast<queryTypes>((int)queryType + 1 % (int)QUERYTYPE_MAX);
+    } while ((hdwId != IS_HARDWARE_TYPE_UNKNOWN) && (devInfo.serialNumber != 0));
+
+    if ((hdwId != IS_HARDWARE_TYPE_UNKNOWN) &&
+        (hdwRunState != ISDevice::HDW_STATE_UNKNOWN) &&
+        (devInfo.protocolVer[0] == PROTOCOL_VERSION_CHAR0)) {
+        comManagerGetData(this, DID_SYS_CMD, 0, 0, 0);
+        comManagerGetData(this, DID_FLASH_CONFIG, 0, 0, 0);
+        comManagerGetData(this, DID_EVB_FLASH_CFG, 0, 0, 0);
+    }
+
+    return true;
+}
+
+bool ISDevice::isOpen() {
+    return serialPortIsOpen(&serialPort);
+}
+
+int ISDevice::writeTo(const unsigned char* buf, int len) {
+    return serialPortWrite(&serialPort, buf, len);
+}
+
+int ISDevice::readFrom(unsigned char* buf, int len) {
+    int bytesRead = serialPortReadTimeout(&serialPort, buf, len, 1);
+
+    if (devLogger) {     // Save raw data to ISlogger
+        // s_is->LogRawData(&s_cm_state->devices[port], bytesRead, buf);
+    }
+    return bytesRead;
+}
+
+void ISDevice::stepComms() {
+    // TODO: This should be moved into the connection manager...
+    //   the device shouldn't have to do any stepping; it should already be done
+    comManagerStepRxInstance(&cmInstance, 0);
+    comManagerStepTxInstance(&cmInstance);
+
+}
+
+void ISDevice::processRxData(p_data_t* data)
+{
+    if (data->hdr.size==0 || data->ptr==NULL) {
+        return;
+    }
+
+    if (devLogger) {
+        // FIXME:  devLogger->SaveData(data, 0);
+        // stepLogFunction(s_cm_state->inertialSenseInterface, data, port);
+    }
+
+    hdwRunState = ISDevice::HDW_STATE_APP;  // It's basically impossible for us to receive ISB protocol, and NOT be in APP state
+    switch (data->hdr.id) {
+        case DID_DEV_INFO:
+            devInfo = *(dev_info_t*)data->ptr;
+            hdwId = ENCODE_DEV_INFO_TO_HDW_ID(devInfo);
+            break;
+        case DID_SYS_CMD:
+            sysCmd = *(system_command_t*)data->ptr;
+            break;
+        case DID_SYS_PARAMS:
+            copyDataPToStructP(&sysParams, data, sizeof(sys_params_t));
+            DEBUG_PRINT("Received DID_SYS_PARAMS\n");
+            break;
+        case DID_FLASH_CONFIG:
+            copyDataPToStructP(&flashCfg, data, sizeof(nvm_flash_cfg_t));
+            if ( dataOverlap(offsetof(nvm_flash_cfg_t, checksum), 4, data)) {
+                sysParams.flashCfgChecksum = flashCfg.checksum;
+            }
+            DEBUG_PRINT("Received DID_FLASH_CONFIG\n");
+            break;
+        case DID_FIRMWARE_UPDATE:
+            // we don't respond to messages if we don't already have an active Updater
+            if (fwUpdater) {
+                fwUpdater->fwUpdate_processMessage(data->ptr, data->hdr.size);
+                fwUpdate();
+            }
+            break;
+
+    // FIXME:  Not sure what the following code is doing... It probably should not be here, and should go away.
+        case DID_GPS1_POS:
+            static time_t lastTime;
+            time_t currentTime = time(NULLPTR);
+            if (abs(currentTime - lastTime) > 5) {	// Update every 5 seconds
+                lastTime = currentTime;
+                gps_pos_t &gps = *((gps_pos_t*)data->ptr);
+                if ((gps.status&GPS_STATUS_FIX_MASK) >= GPS_STATUS_FIX_3D) {
+                    // *s_cm_state->clientBytesToSend = nmea_gga(s_cm_state->clientBuffer, s_cm_state->clientBufferSize, gps);
+                }
+            }
+            break;
+    }
+
+    // Now that we've done our own processing, we can call others...
+    if (binaryCallbacks[data->hdr.id].callback != NULLPTR) {
+        binaryCallbacks[data->hdr.id].callback(this, data);
+    }
+
+    if (binaryCallbackGlobal != NULLPTR) {
+        // Called for all DID's
+        binaryCallbackGlobal(this, data);
+    }
+}
+
+// return 0 on success, -1 on failure
+void ISDevice::processRxNmea(const uint8_t* msg, int msgSize) {
+    if (m_handlerNmea) {
+        m_handlerNmea(this, msg, msgSize);
+    }
+
+    switch (getNmeaMsgId(msg, msgSize))
+    {
+        case NMEA_MSG_ID_INFO:
+            nmea_parse_info(devInfo, (const char*)msg, msgSize);
+            hdwId = ENCODE_DEV_INFO_TO_HDW_ID(devInfo);
+            hdwRunState = ISDevice::HDW_STATE_APP;
+        break;
+    }
+}
+
+void ISDevice::getData(uint16_t did, uint16_t size, uint16_t offset, uint16_t period) {
+    // Create and Send request packet
+    p_data_get_t get;
+    get.id = did;
+    get.offset = offset;
+    get.size = size;
+    get.period = period;
+
+    comManagerSendInstance(cmInstance, serialPort, PKT_TYPE_GET_DATA, &get, 0, sizeof(get), 0);
 }
--- src/ISDevice.h
+++ src/ISDevice.h
@@ -15,6 +15,8 @@
 #include "protocol/FirmwareUpdate.h"
 #include "ISFirmwareUpdater.h"
 
+#include <functional>
+
 extern "C"
 {
     // [C COMM INSTRUCTION]  Include data_sets.h and com_manager.h
@@ -22,6 +24,14 @@ extern "C"
     #include "com_manager.h"
     #include "serialPortPlatform.h"
 }
+
+
+#define PRINT_DEBUG 0
+#if PRINT_DEBUG
+#define DEBUG_PRINT(...)    printf("L%d: ", __LINE__); printf(__VA_ARGS__)
+#else
+#define DEBUG_PRINT(...)
+#endif
 
 class ISFirmwareUpdater;
 
@@ -35,6 +45,7 @@ public:
 
     int portHandle = 0;
     serial_port_t serialPort = { };
+    CMHANDLE cmInstance = { 0 };
     // libusb_device* usbDevice = nullptr; // reference to the USB device (if using a USB connection), otherwise should be nullptr.
 
     uint16_t hdwId;                         //! hardware type and version (ie, IMX-5.0)
@@ -85,14 +96,115 @@ public:
         sysParams.flashCfgChecksum = 0xFFFFFFFF;		// Invalidate flash config checksum to trigger sync event
     }
 
+    /**
+     * Associates this IS device with the specified serial port instance. This call is used when a port is
+     * known to have a device, but the device itself is not known. This will associate which ever device is
+     * connected to the specified port, even if that device is different from the current instance's device.
+     * @param port
+     * @return true if a valid device was discovered and bound, otherwise false
+     */
+    bool bindDevice(serial_port_t& port);
+
+    /**
+     * Attempts to locate the port which hosts this device. This call is used to find a specific device from
+     * a set of ports, by querying each available port and checking whether the device on that port matches
+     * the current device information. If the device is found on one of the provided port, it is bound to
+     * that port.
+     * @return true if the device was found and successfully bound to the port, otherwise false.
+     */
+    bool findDevice(std::vector<serial_port_t&> ports);
+
+    /**
+     * Opens the bound port, and enables communication with the device.  NOTE: THIS MAYBE UNNECESSARY as the
+     * port is likely already opened when initially bound. Ultimately, we want the connection opened/close
+     * and the device independent of that connection, ie, device.connection.open(...)
+     * @param baudRate the baud rate to open the connection at
+     * @param validate performs device comms validation after opening the port; ensures that the device is alive and well
+     * @return true if successfully opened, otherwise false
+     */
+    bool open(int baudRate, bool validate);
+
+    /**
+     * @return true if this device has an option port/connection, otherwise false
+     */
+    bool isOpen();
+
+    int writeTo(const unsigned char* buf, int len);
+
+    int readFrom(unsigned char* buf, int len);
+
+    /**
+     * Closes the current connection/port.
+     */
+    void close();
+
+    /**
+     * Queries the device info from the bound port, and populates devInfo.
+     * @return true if the device was successfully queried, and devInfo populated, otherwise false
+     */
     bool queryDeviceInfo();
+
+    bool stopBroadcasts(bool allPorts = false);
+    bool enableBroadcast(int did, int period);
+    bool sendReset();
 
 protected:
     bool handshakeISB();
     bool queryDeviceInfoISB();
-
     bool queryDeviceInfoDFU();
+    bool validateDevice(uint32_t timeout);
 
+    void processRxData(p_data_t* data);
+    void processRxNmea(const uint8_t* msg, int msgSize);
+
+    void getData(uint16_t did, uint16_t size, uint16_t offset, uint16_t period);
+
+    void stepComms();
+
+private:
+    typedef std::function<void(ISDevice* device, p_data_t* data)> pfnHandleBinaryData;
+    typedef void(*pfnStepLogFunction)(ISDevice* device, const p_data_t* data);
+    typedef int(*pfnDeviceRmcHandler)(port_handle_t port, p_data_get_t* req);
+    typedef int(*pfnDeviceGenMsgHandler)(ISDevice* device, const unsigned char* msg, int msgSize);
+    typedef int(*pfnDeviceParseErrorHandler)(ISDevice* device, is_comm_instance_t* comm);
+
+    enum queryTypes {
+        QUERYTYPE_NMEA = 0,
+        QUERYTYPE_ISB,
+        QUERYTYPE_IBbootloader,
+        QUERYTYPE_mcuBoot,
+        QUERYTYPE_MAX = QUERYTYPE_mcuBoot,
+    };
+
+    std::vector<broadcast_msg_t> broadcastMsgs;
+
+    typedef struct {
+        uint8_t updated;
+        uint32_t lastRxTime;
+        pfnHandleBinaryData callback;
+    } did_info_t;
+
+    pfnHandleBinaryData binaryCallbackGlobal;
+    std::map<int, did_info_t> binaryCallbacks;
+
+    uint32_t m_timeMs;
+    pfnDeviceRmcHandler         m_handlerRmc = NULLPTR;
+    pfnDeviceGenMsgHandler      m_handlerNmea = NULLPTR;
+    pfnDeviceGenMsgHandler      m_handlerUblox = NULLPTR;
+    pfnDeviceGenMsgHandler      m_handlerRtcm3 = NULLPTR;
+    pfnDeviceGenMsgHandler      m_handlerSpartn = NULLPTR;
+    pfnDeviceParseErrorHandler  m_handlerError = NULLPTR;
+
+    pfnStepLogFunction stepLogFunction;
+
+    struct {
+        is_comm_instance_t comm;            //! Comm instances
+        uint8_t comm_buffer[PKT_BUF_SIZE];  //! Comm instance data buffer
+
+        #if ENABLE_PACKET_CONTINUATION
+        p_data_t con;       //! Continuation data for packets
+        #endif
+    } cmPort;
 };
 
 
