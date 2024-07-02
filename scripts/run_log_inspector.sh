1b37aad967ae9793c88f960efd4c0a54774a2cd7
waltjohnson <walt@inertialsense.com>
2024-07-02 12:42:16 -0600

    Add version requirement for allantools and made sure run log inspector scripts use venv

--- scripts/run_log_inspector.sh
+++ scripts/run_log_inspector.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+source "$(dirname "$(realpath $0)")/lib/python_venv.sh" # Load python virtual enviroment must be ran before pushd
 pushd "$(dirname "$(realpath $0)")" > /dev/null
 
 python3 ../python/logInspector/logInspectorInternal.py
