fbfb03f8cd5a54b32e038c2c33759de30a2527df
Kyle Mallory <kyle@inertialsense.com>
2024-07-15 17:45:30 -0600

    Missed portName() call in ISFirmwareUpdater related stuff.

--- cltool/src/cltool_main.cpp
+++ cltool/src/cltool_main.cpp
@@ -581,7 +581,7 @@ void cltool_firmwareUpdateInfo(void* obj, ISBootloader::eLogLevel level, const c
     } else {
         ISFirmwareUpdater *fwCtx = (ISFirmwareUpdater *) obj;
         if (buffer[0] || (((g_commandLineOptions.displayMode != cInertialSenseDisplay::DMODE_QUIET) && (fwCtx->fwUpdate_getSessionStatus() == fwUpdate::IN_PROGRESS)))) {
-            printf("[%5.2f] [%s:SN%07d > %s]", current_timeMs() / 1000.0f, fwCtx->portName, fwCtx->devInfo->serialNumber, fwCtx->fwUpdate_getSessionTargetName());
+            printf("[%5.2f] [%s:SN%07d > %s]", current_timeMs() / 1000.0f, portName(fwCtx->port), fwCtx->devInfo->serialNumber, fwCtx->fwUpdate_getSessionTargetName());
             if (fwCtx->fwUpdate_getSessionStatus() == fwUpdate::IN_PROGRESS) {
                 int tot = fwCtx->fwUpdate_getTotalChunks();
                 int num = fwCtx->fwUpdate_getNextChunkID();
