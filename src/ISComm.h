79ec46f846055fb05e6e5da01d89eb053cdd26f4
waltjohnson <walt@inertialsense.com>
2024-07-21 01:31:18 -0600

    More cleanup

--- src/ISComm.h
+++ src/ISComm.h
@@ -638,14 +638,14 @@ typedef struct
 {
     pfnIsCommIsbDataHandler         isbData;    // Message handler - Inertial Sense binary (ISB) data message 
     pfnIsCommHandler                isb;        // Message handler - Inertial Sense binary (ISB) general message 
-    pfnIsCommGenMsgHandler          nmea;   	// Message handler - NMEA
-    pfnIsCommGenMsgHandler          ublox;  	// Message handler - Ublox
-    pfnIsCommGenMsgHandler          rtcm3;  	// Message handler - RTCM3
+    pfnIsCommGenMsgHandler          nmea;       // Message handler - NMEA
+    pfnIsCommGenMsgHandler          ublox;      // Message handler - Ublox
+    pfnIsCommGenMsgHandler          rtcm3;      // Message handler - RTCM3
     pfnIsCommGenMsgHandler          sony;  	    // Message handler - Sony
-    pfnIsCommGenMsgHandler          sprtn;  	// Message handler - SPARTN
-    pfnIsCommParseErrorHandler      error;	    // Error handler 
+    pfnIsCommGenMsgHandler          sprtn;      // Message handler - SPARTN
+    pfnIsCommParseErrorHandler      error;      // Error handler 
     pfnIsCommHandler                all;        // Message handler - Called for all messages in addition to any message handler including the error handler.
-    pfnIsCommAsapMsg                rmc;    	// Message handler - Used in com_manager to forward data requests to realtime message controller (RMC).  Called whenever we get a message broadcast request or message disable command.
+    pfnIsCommAsapMsg                rmc;        // Message handler - Used in com_manager to forward data requests to realtime message controller (RMC).  Called whenever we get a message broadcast request or message disable command.
 } is_comm_callbacks_t;
 
 /**
