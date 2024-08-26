5a354692928702b7966977752af1072c25121886
tmcd249 <tmcd249@msn.com>
2024-08-26 15:36:49 -0600

    fix compiler erros

--- src/ISDataMappings.cpp
+++ src/ISDataMappings.cpp
@@ -1187,8 +1187,12 @@ static void PopulateGpxStatusMappings(map_name_to_info_t mappings[DID_COUNT])
     ADD_MAP(m, totalSize, "navOutputPeriodMs", navOutputPeriodMs, 0, DataTypeUInt32, uint32_t, 0);
     ADD_MAP(m, totalSize, "flashCfgChecksum", flashCfgChecksum, 0, DataTypeUInt32, uint32_t, 0);
     ADD_MAP(m, totalSize, "rtkMode", rtkMode, 0, DataTypeUInt32, uint32_t, 0);
-    ADD_MAP(m, totalSize, "gnss1RunState", gnss1RunState, 0, DataTypeUInt32, uint32_t, 0);
-    ADD_MAP(m, totalSize, "gnss2RunState", gnss2RunState, 0, DataTypeUInt32, uint32_t, 0);
+    ADD_MAP(m, totalSize, "gnss1.RunState", gnsssStatus[0].runState, 0, DataTypeUInt8, uint8_t, 0);
+    ADD_MAP(m, totalSize, "gnss1.fwUpdateState", gnsssStatus[0].fwUpdateState, 0, DataTypeUInt8, uint8_t, 0);
+    ADD_MAP(m, totalSize, "gnss1.initState", gnsssStatus[0].initState, 0, DataTypeUInt8, uint8_t, 0);
+    ADD_MAP(m, totalSize, "gnss2.RunState", gnsssStatus[1].runState, 0, DataTypeUInt8, uint8_t, 0);
+    ADD_MAP(m, totalSize, "gnss2.fwUpdateState", gnsssStatus[1].fwUpdateState, 0, DataTypeUInt8, uint8_t, 0);
+    ADD_MAP(m, totalSize, "gnss2.initState", gnsssStatus[1].initState, 0, DataTypeUInt8, uint8_t, 0);
     ADD_MAP(m, totalSize, "SourcePort", gpxSourcePort, 0, DataTypeUInt8, uint8_t, 0);
     ADD_MAP(m, totalSize, "upTime", upTime, 0, DataTypeDouble, double, 0);
 }
