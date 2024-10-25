8b87aa1ebccf0b31b013f306c3210e7b4e81f2bb
Kyle Mallory <kyle@inertialsense.com>
2024-10-25 17:11:03 -0600

    Windows is dumb, still.

--- src/util/util.cpp
+++ src/util/util.cpp
@@ -18,6 +18,31 @@
 
 #include "ISDataMappings.h"
 
+
+#ifdef PLATFORM_IS_WINDOWS
+#include <time.h>
+#include <iomanip>
+#include <sstream>
+
+extern "C" char* strptime(const char* s,
+                          const char* f,
+                          struct tm* tm) {
+    // Isn't the C++ standard lib nice? std::get_time is defined such that its
+    // format parameters are the exact same as strptime. Of course, we have to
+    // create a string stream first, and imbue it with the current C locale, and
+    // we also have to make sure we return the right things if it fails, or
+    // if it succeeds, but this is still far simpler an implementation than any
+    // of the versions in any of the C standard libraries.
+    std::istringstream input(s);
+    input.imbue(std::locale(setlocale(LC_ALL, nullptr)));
+    input >> std::get_time(tm, f);
+    if (input.fail()) {
+        return nullptr;
+    }
+    return (char*)(s + input.tellg());
+}
+#endif
+
 std::string utils::getCurrentTimestamp() {
     using std::chrono::system_clock;
     auto currentTime = std::chrono::system_clock::now();
