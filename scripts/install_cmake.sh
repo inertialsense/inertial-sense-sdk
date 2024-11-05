5eadb41bd00682215c51795f2311796d163640b5
Kyle Mallory <kyle@inertialsense.com>
2024-11-05 14:38:59 -0700

    Fixing CMake docker scripts, still.

--- scripts/install_cmake.sh
+++ scripts/install_cmake.sh
@@ -9,7 +9,6 @@ echo_blue "==============================================="
 
 curl -o /tmp/cmake-install.sh -L https://github.com/Kitware/CMake/releases/download/v3.26.0/cmake-3.26.0-linux-x86_64.sh \
       && chmod u+x /tmp/cmake-install.sh \
-      && mkdir /usr/bin \
       && /tmp/cmake-install.sh --skip-license --prefix=/usr/bin \
       && rm /tmp/cmake-install.sh
 
