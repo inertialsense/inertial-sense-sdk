04eaa3c9d6f63f111cb01e531b7dea0761d7a8fa
Kyle Mallory <kyle@inertialsense.com>
2024-11-05 12:44:12 -0700

    scripts/step to build newer CMake for ROS docker images.

---
+++ scripts/install_cmake.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+function echo_blue   { echo -e "\033[1;34m$@\033[0m"; }
+
+pushd "$(dirname "$(realpath $0)")" > /dev/null
+
+echo_blue "==============================================="
+echo_blue " CMake 3.26.0 (from source)                    "
+echo_blue "==============================================="
+
+wget https://github.com/Kitware/CMake/releases/download/v3.26.0/cmake-3.26.0-linux-x86_64.sh \
+      -q -O /tmp/cmake-install.sh \
+      && chmod u+x /tmp/cmake-install.sh \
+      && mkdir /usr/bin/cmake \
+      && /tmp/cmake-install.sh --skip-license --prefix=/usr/bin/cmake \
+      && rm /tmp/cmake-install.sh
+
+source ~/.bashrc
+popd > /dev/null
