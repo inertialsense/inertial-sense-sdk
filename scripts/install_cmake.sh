131c6e8254fae992c6b9b8d3d9e1cbe5ac37500b
Kyle Mallory <kyle@inertialsense.com>
2024-11-05 13:15:27 -0700

    Giving up on wget

--- scripts/install_cmake.sh
+++ scripts/install_cmake.sh
@@ -7,13 +7,10 @@ echo_blue "==============================================="
 echo_blue " CMake 3.26.0 (from source)                    "
 echo_blue "==============================================="
 
-sudo apt -y install wget
-wget https://github.com/Kitware/CMake/releases/download/v3.26.0/cmake-3.26.0-linux-x86_64.sh \
-      -q -O /tmp/cmake-install.sh \
+curl -o /tmp/cmake-install.sh -L https://github.com/Kitware/CMake/releases/download/v3.26.0/cmake-3.26.0-linux-x86_64.sh \
       && chmod u+x /tmp/cmake-install.sh \
       && mkdir /usr/bin/cmake \
       && /tmp/cmake-install.sh --skip-license --prefix=/usr/bin/cmake \
       && rm /tmp/cmake-install.sh
 
-[[ -f ~/.bashrc ]] || source ~/.bashrc
 popd > /dev/null
