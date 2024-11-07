2785df8e64505e14b33017d2d0064d2344788005
waltjohnson <walt@inertialsense.com>
2024-11-07 02:40:03 -0700

    Switch [ pass ] to [PASSED]

--- python/supernpp/supernpp.py
+++ python/supernpp/supernpp.py
@@ -97,7 +97,7 @@ class SuperNPP():
                     self.log.calculateRMS()
                     passRMS = self.log.printRMSReport()
                     if passRMS == 1:
-                        f.write("[ pass ] " + sdir + "\n")
+                        f.write("[PASSED] " + sdir + "\n")
                         self.rmsPassResults.append(sdir)
                     else:
                         f.write("[FAILED] " + sdir + "\n")
@@ -279,7 +279,7 @@ if __name__ == "__main__":
     testSummaryFilename = directory+"/test_summary.txt"
     nppPrint("\n")
     nppPrint("====================  Super NPP Results  ====================")
-    print_case(testSummaryFilename, "  RMS Test PASSED:", "[ pass ]")
+    print_case(testSummaryFilename, "  RMS Test PASSED:", "[PASSED]")
     print_case(testSummaryFilename, "  RMS Test FAILED:", "[FAILED]")
     print_case(testSummaryFilename, "  Failed to Reprocess:", "[NODATA]")
     nppPrint("=============================================================")
