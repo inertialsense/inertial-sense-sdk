93163ee6dcd82be1be64f16d23f4242b47d08278
waltjohnson <walt@inertialsense.com>
2024-11-13 17:47:15 -0700

    Cleanup

--- scripts/windows/install_log_inspector_dependencies.bat
+++ scripts/windows/install_log_inspector_dependencies.bat
@@ -6,20 +6,15 @@ echo.
 :: Set SDK_DIR as directory path
 for %%i in (%~dp0..\..) do SET SDK_DIR=%%~fi
 
-:: Build SDK cpp needed by LogInspector
-call %SDK_DIR%\scripts\windows\build_is_sdk.bat
-
 :: Install dependencies
 pushd %SDK_DIR%\python\
 pip3 install setuptools pybind11 wheel
-::pip3 install logInspector/
 popd 
 if %errorlevel% neq 0 ( echo Error installing Log Inspector dependencies! & exit /b %errorlevel% )
 
-:: Build Log Inspector locally (which also installs dependencies)
-pushd %SDK_DIR%\python
-python setup.py build_ext --inplace
+pushd %SDK_DIR%\python\
+pip3 install logInspector/
 popd
-if %errorlevel% neq 0 ( echo Error building Log Inspector locally! & exit /b %errorlevel% )
+if %errorlevel% neq 0 ( echo Error installing Log Inspector dependencies! & exit /b %errorlevel% )
 
 pause
