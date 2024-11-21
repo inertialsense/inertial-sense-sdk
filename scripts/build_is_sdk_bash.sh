754991e246bcfc24581a3febefd95ff4ee52ba36
waltjohnson <walt@inertialsense.com>
2024-11-20 17:21:43 -0700

    Do not use python for evaltool build

---
+++ scripts/build_is_sdk_bash.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+source ${SCRIPT_DIR}/lib/echo_color.sh
+source ${SCRIPT_DIR}/lib/results_build.sh
+source ${SCRIPT_DIR}/lib/results_tests.sh
+
+pushd "$(dirname "$(realpath $0)")" > /dev/null
+
+BUILD_TYPE=Release
+CLEAN="false"
+
+for arg in "$@"; do
+    case $arg in
+        -c|--clean)
+            CLEAN="true"
+            ;;
+        -d|--debug)
+            BUILD_TYPE=Debug
+            ;;
+    esac
+done
+
+pushd .. > /dev/null
+
+if [ "${CLEAN}" == "true" ]; then
+    echo -e "\n\n=== Running make clean... ==="
+    make clean 2> /dev/null
+    rm -rf build
+    build_result=$?
+else
+    echo -e "\n\n=== Running make... (${BUILD_TYPE} ==="
+    # cmake -B build -S . -DCMAKE_BUILD_TYPE=${BUILD_TYPE} && make -j`nproc` -l`nproc`
+    cmake -B build -S . -DCMAKE_BUILD_TYPE=${BUILD_TYPE} && cmake --build build --config ${BUILD_TYPE} -j `nproc`
+
+    build_result=$?
+fi
+
+popd > /dev/null
+
+# Return results: 0 = pass, 0 != fail
+exit ${build_result}
