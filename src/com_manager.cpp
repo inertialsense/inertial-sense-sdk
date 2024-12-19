b5e85dccd152b04e62c61d48a18e4cc5fd5fc6f0
tmcd249 <tmcd249@msn.com>
2024-12-19 10:46:06 -0700

    code readabilty

--- src/com_manager.cpp
+++ src/com_manager.cpp
@@ -588,7 +588,8 @@ int ISComManager::processBinaryRxPacket(protocol_type_t ptype, packet_t *pkt, po
             p_data_get_t *gdata = ((p_data_get_t *) (pkt->data.ptr));
             // Forward to gpx
             if (IO_CONFIG_GPS1_TYPE(g_nvmFlashCfg->ioConfig) == IO_CONFIG_GPS_TYPE_GPX &&
-                (((gdata->id >= DID_GPX_FIRST) && (gdata->id <= DID_GPX_LAST)) || (gdata->id == DID_RTK_DEBUG))) {
+                (((gdata->id >= DID_GPX_FIRST) && (gdata->id <= DID_GPX_LAST)) || (gdata->id == DID_RTK_DEBUG))) 
+            {
                 comManagerGetData(COM0_PORT, gdata->id, gdata->size, gdata->offset, gdata->period);
 
                 if (gdata->id == DID_RTK_DEBUG) {
