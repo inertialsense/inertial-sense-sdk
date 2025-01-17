aee184fadff2e16ddccf1c9411453d5caff5a7ac
tmcd249 <tmcd249@msn.com>
2025-01-17 09:03:35 -0700

    bump

--- ROS/ros2/src/inertial_sense_ros2.cpp
+++ ROS/ros2/src/inertial_sense_ros2.cpp
@@ -1047,7 +1047,7 @@ void InertialSenseROS::configure_rtk()
         {
             RCLCPP_INFO(rclcpp::get_logger("config_rtk_rover_w_radio"),"InertialSenseROS: Configuring RTK Rover with radio enabled");
             if (RTK_base_) RTK_base_->enable = false;
-            rtkConfigBits_ |= (rs_.gps1.type == "F9P" ? RTK_CFG_BITS_ROVER_MODE_RTK_POSITIONING_NEW : RTK_CFG_BITS_ROVER_MODE_RTK_POSITIONING_DAPRECATED);
+            rtkConfigBits_ |= (rs_.gps1.type == "F9P" ? RTK_CFG_BITS_ROVER_MODE_RTK_POSITIONING_NEW : RTK_CFG_BITS_ROVER_MODE_RTK_POSITIONING_DEPRECATED);
             SET_CALLBACK(DID_GPS1_RTK_POS_MISC, gps_rtk_misc_t, RTK_Misc_callback, rs_.rtk_pos.period);
             SET_CALLBACK(DID_GPS1_RTK_POS_REL, gps_rtk_rel_t, RTK_Rel_callback, rs_.rtk_pos.period);
         }
@@ -1055,7 +1055,7 @@ void InertialSenseROS::configure_rtk()
         {
             RCLCPP_INFO(rclcpp::get_logger("config_as_rtk_rover"),"InertialSenseROS: Configuring as RTK Rover");
             if (RTK_base_) RTK_base_->enable = false;
-            rtkConfigBits_ |= (rs_.gps1.type == "F9P" ? RTK_CFG_BITS_ROVER_MODE_RTK_POSITIONING_NEW : RTK_CFG_BITS_ROVER_MODE_RTK_POSITIONING_DAPRECATED);
+            rtkConfigBits_ |= (rs_.gps1.type == "F9P" ? RTK_CFG_BITS_ROVER_MODE_RTK_POSITIONING_NEW : RTK_CFG_BITS_ROVER_MODE_RTK_POSITIONING_DEPRECATED);
             connect_rtk_client((RtkRoverCorrectionProvider_Ntrip&)*RTK_rover_->correction_input);
             SET_CALLBACK(DID_GPS1_RTK_POS_MISC, gps_rtk_misc_t, RTK_Misc_callback, rs_.rtk_pos.period);
             SET_CALLBACK(DID_GPS1_RTK_POS_REL, gps_rtk_rel_t, RTK_Rel_callback, rs_.rtk_pos.period);
