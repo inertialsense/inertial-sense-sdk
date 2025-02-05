901301703dfe56b079dda44effd8461228b00d90
tmcd249 <tmcd249@msn.com>
2025-02-05 16:54:04 -0700

    fix error

--- cltool/src/cltool_main.cpp
+++ cltool/src/cltool_main.cpp
@@ -279,16 +279,16 @@ void cltool_requestDataSets(InertialSense& inertialSenseInterface, std::vector<s
     for (stream_did_t& dataItem : datasets)
     {   // Datasets to stream
         inertialSenseInterface.BroadcastBinaryData(dataItem.did, dataItem.periodMultiple);
+        
+        system_command_t cfg;
         switch (dataItem.did)
         {
             case DID_RTOS_INFO:
-                system_command_t cfg;
                 cfg.command = SYS_CMD_ENABLE_RTOS_STATS;
                 cfg.invCommand = ~cfg.command;
                 inertialSenseInterface.SendRawData(DID_SYS_CMD, (uint8_t*)&cfg, sizeof(system_command_t), 0);
                 break;
             case DID_GPX_RTOS_INFO:
-                system_command_t cfg;
                 cfg.command = SYS_CMD_GPX_ENABLE_RTOS_STATS;
                 cfg.invCommand = ~cfg.command;
                 inertialSenseInterface.SendRawData(DID_SYS_CMD, (uint8_t*)&cfg, sizeof(system_command_t), 0);
