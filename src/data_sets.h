485a2ac519d4f352712332962e03f4edee37a97f
tmcd249 <tmcd249@msn.com>
2025-04-03 13:08:21 -0600

    changed comment

--- src/data_sets.h
+++ src/data_sets.h
@@ -1568,8 +1568,11 @@ enum eSystemCommand
     SYS_CMD_TEST_SER0_TX_PP_U                           = 81,           // (uint32 inv: 4294967214)
     SYS_CMD_TEST_SER0_TX_PP_D                           = 82,           // (uint32 inv: 4294967213)
 
-    // Experimental commands to help debug TX->RX bug (TM) TODO: Remove/refactor depending on the result.
-    SYS_CMD_SET_OUTPUT_PIN_TO_TRISTATE                  = 96,           // (uint32 inv: 4294967199)
+    // Experimental commands debuging TX->RX bug (TM) 
+    // TODO: Action date (after 8/8/25): 
+    //  A: Remove if does not fix tx->rx bug. 
+    //  B: If it does help consider expanding to all pins to prevent from happening.
+    SYS_CMD_OUTPUT_IDLE                  = 96,           // (uint32 inv: 4294967199)
     
     SYS_CMD_SAVE_FLASH                                  = 97,           // (uint32 inv: 4294967198)
     SYS_CMD_SAVE_GPS_ASSIST_TO_FLASH_RESET              = 98,           // (uint32 inv: 4294967197)
