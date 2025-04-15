7558bf759a9e6be133ce886528436297a684037e
tmcd249 <tmcd249@msn.com>
2025-04-15 15:45:47 -0600

    what does this do

--- src/protocol_nmea.cpp
+++ src/protocol_nmea.cpp
@@ -281,7 +281,14 @@ char *ASCII_to_u32(uint32_t *val, char *ptr)
 
 char *ASCII_to_u64(uint64_t *val, char *ptr)
 {
-    val[0] = (uint64_t)atol(ptr);	ptr = ASCII_find_next_field(ptr);
+// #if PLATFORM_IS_WINDOWS
+    char *endPtr = ptr;
+    val[0] = (uint64_t)std::strtoll(ptr, &endPtr, 10); 
+    ptr = ASCII_find_next_field(ptr);
+// #else
+//     val[0] = (uint64_t)atol(ptr); 
+//     ptr = ASCII_find_next_field(ptr);
+// #endif
     return ptr;
 }
 
@@ -1270,7 +1277,6 @@ int nmea_powPrep(char a[], int startN, const int aSize, gps_pos_t &pos)
     nmea_sprint(a, aSize, n, ",%d", valid);                 // 1
     nmea_sprint(a, aSize, n, ",%d", pos.week);              // 2
     nmea_sprint(a, aSize, n, ",%" PRIu64, ((uint64_t)pos.timeOfWeekMs)*1000); // 3
-    // nmea_sprint(a, aSize, n, ",%I64u", ((long)pos.timeOfWeekMs)*1000); // 3  
 
     valid = (pos.leapS > 10 && pos.leapS < 30) ? 1 : 0;     // should be ~18 so give a little leeway
     nmea_sprint(a, aSize, n, ",%d", valid);                 // 4
