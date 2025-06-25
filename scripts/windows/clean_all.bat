577473955195d1bb2c7ebf399e40c0e766681160
waltjohnson <walt@inertialsense.com>
2025-06-25 12:10:03 -0600

    Add all parameters to clean_all scripts

--- scripts/windows/clean_all.bat
+++ scripts/windows/clean_all.bat
@@ -2,7 +2,7 @@
 for %%i in (%~dp0..\..) do SET SDK_DIR=%%~fi
 call %SDK_DIR%\scripts\lib\activate_python_venv.bat
 
-python %SDK_DIR%\scripts\build_all.py --clean
+python %SDK_DIR%\scripts\build_all.py --clean %*
 
 :: Check the exit code and exit with the same code if an error occurred
 IF ERRORLEVEL 1 EXIT /B %ERRORLEVEL%
