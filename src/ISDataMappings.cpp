61d72d22b03691cb029102718a4a099a98c65213
waltjohnson <walt@inertialsense.com>
2025-07-13 05:58:08 -0600

    Include sstring for Windows

--- src/ISDataMappings.cpp
+++ src/ISDataMappings.cpp
@@ -20,6 +20,7 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 #include <stdio.h>
 #include <assert.h>
 #include <string.h>
+#include <sstream>
 
 #include "ISDataMappings.h"
 #include "DataJSON.h"
