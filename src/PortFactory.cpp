03cac8751ad9f65c99795f7a2872e4b77d43a42b
waltjohnson <walt@inertialsense.com>
2025-07-28 02:48:47 -0600

    Fix build warning

--- src/PortFactory.cpp
+++ src/PortFactory.cpp
@@ -82,7 +82,12 @@ void SerialPortFactory::locatePorts(std::function<void(PortFactory*, uint16_t, s
 }
 
 int SerialPortFactory::onPortError(port_handle_t port, int errCode, const char *errMsg) {
-    printf("%s :: Error %d : %s\n", portName(port), errCode, (errMsg ? errMsg : "(null)"));
+    const char* portStr = portName(port);
+    const char* safeErrMsg = errMsg ? errMsg : "";
+    // Split the printf into two calls (helps avoid inlining inference)
+    printf("%s :: Error %d : ", portStr, errCode);
+    printf("%s\n", safeErrMsg);
+
     // decide which of these should result in a port-closure, vs a port invalid, vs nothing...
     switch (errCode) {
 
