f71e5461900f5564c197ceeffc8db550367b670f
firiusfoxx <firiusfoxx@yahoo.com>
2025-08-06 14:07:56 -0600

    Now for Windows support - Eleventh fix (Add -DCURL_STATICLIB to cltool)
    Add -DYAML_CPP_STATIC_DEFINE -DCURL_STATICLIB to include_is_sdk_target_link_libraries.cmake
    Remove -DYAML_CPP_STATIC_DEFINE from cltool CMakeLists.txt

--- include_is_sdk_target_link_libraries.cmake
+++ include_is_sdk_target_link_libraries.cmake
@@ -4,6 +4,7 @@ find_package(Threads REQUIRED)
 # Add Ws2_32 for networking
 if(WIN32)
     target_link_libraries(${PROJECT_NAME} Ws2_32)
+    add_definitions(-DYAML_CPP_STATIC_DEFINE -DCURL_STATICLIB)
 endif()
 
 # Link InertialSenseSDK static library to the executable
