0b23eefb0857eeeafccd81fcb5ee40430fd60dc2
tmcd249 <tmcd249@msn.com>
2025-09-22 16:21:06 -0600

    it compiles but does not link yet

--- src/com_manager.cpp
+++ src/com_manager.cpp
@@ -350,7 +350,7 @@ void ISComManager::getData(port_handle_t port, uint16_t did, uint16_t size, uint
         // depending on the nature of the error, we may want to close the port.
         // FIXME: we really should be more selective with which errors we actually close the port for.
         if (SERIAL_PORT(port)->errorCode > 0) {
-#if !(defined(IMX_5) || defined(IMX_6))
+    #if !(defined(IMX_5) || defined(IMX_6))
             switch (SERIAL_PORT(port)->errorCode) {
                 case ENODEV:        // no such device, so no need to close it
                     break;
@@ -361,7 +361,6 @@ void ISComManager::getData(port_handle_t port, uint16_t did, uint16_t size, uint
             // removePort(port);
             // memset(SERIAL_PORT(port), 0, sizeof(serial_port_s));
             // TODO: we still haven't deleted all references to the port, and this will likely cause problems (ie, InertialSense class, etc).
-    #endif
     #endif
         }
     }
