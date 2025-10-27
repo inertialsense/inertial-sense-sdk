3551944650705d50a426193bf9609bb987753721
waltjohnson <walt@inertialsense.com>
2025-10-27 12:37:40 -0600

    Consolidate install_python_dependencies.bat into SDK

--- scripts/windows/install_python_dependencies.bat
+++ scripts/windows/install_python_dependencies.bat
@@ -4,5 +4,9 @@
 for %%i in (%~dp0..\..) do SET SDK_DIR=%%~fi
 call %SDK_DIR%\scripts\lib\activate_python_venv.bat
 
+echo .
+echo Installing Python dependencies C...
+echo .
+
 python -m pip install --upgrade pip
-python -m pip install gitpython requests ruamel.yaml semver PyYAML
+python -m pip install gitpython requests ruamel.yaml semver setuptools PyYAML
