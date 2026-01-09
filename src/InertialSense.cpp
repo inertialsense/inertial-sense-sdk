f671e3772402ccbe51d66eec900372c472f9dcbc
Kyle Mallory <kyle@inertialsense.com>
2026-01-09 11:27:33 -0700

    Missed C++20 issue for Windows.

--- src/InertialSense.cpp
+++ src/InertialSense.cpp
@@ -1544,7 +1544,7 @@ void InertialSense::SetEventFilter(int target, uint32_t msgTypeIdMask, uint8_t p
     event.time = 123,
     event.senderSN = 0,
     event.senderHdwId = 0,
-    event.length = sizeof(did_event_filter_t),
+    event.length = sizeof(did_event_filter_t);
 
     did_event_filter_t filter = {};
     filter.portMask = portMask;
