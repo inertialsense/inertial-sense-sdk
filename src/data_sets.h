1c6f0e0e486626fda04ba75b96a2b435b1ea9315
Alexander Bogdanov <abogdanov@inertialsense.com>
2026-02-18 18:45:40 -0800

    padding fix

--- src/data_sets.h
+++ src/data_sets.h
@@ -4026,9 +4026,8 @@ typedef struct PACKED
     uint8_t qualP[NFREQ+NEXOBS];
 
     /** reserved, for alignment */
-    uint8_t reserved;
-#if NFREQ == 2
-    uint8_t reserved1;
+#if NFREQ == 3
+    uint8_t reserved[3];
 #endif
 
     /** Observation data carrier-phase (cycle). The carrier phase initial ambiguity is initialized using an approximate value to make the magnitude of the phase close to the pseudorange measurement. Clock resets are applied to both phase and code measurements in accordance with the RINEX specification. */
