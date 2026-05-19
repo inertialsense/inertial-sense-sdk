60a6b46571bec6b04b46b70236f5b9cd76f970f1
waltjohnson <walt@inertialsense.com>
2026-05-19 09:36:13 -0600

    Added RTOS task period overrun to GPX

--- src/data_sets.h
+++ src/data_sets.h
@@ -1609,6 +1609,8 @@ enum eGenFaultCodes
     GFC_GNSS_GENERAL_FAULT                  = 0x08000000,
     /** Fault: Invalid IMU input rejected by EKF */
     GFC_EKF_INPUT_INVALID_IMU               = 0x10000000,
+    /** Fault: GPS RTOS error */
+    GFC_GNSS_RTOS_ERROR                     = 0x20000000,
 
     /** IMX GFC flags that relate to GPX status flags */
     GFC_GPX_STATUS_COMMON_MASK = GFC_GNSS1_INIT | GFC_GNSS2_INIT | GFC_GNSS_TX_LIMITED | GFC_GNSS_RX_OVERRUN | GFC_GNSS_CRITICAL_FAULT | GFC_GNSS_RECEIVER_TIME | GFC_GNSS_GENERAL_FAULT,
@@ -4826,6 +4828,8 @@ enum eGpxStatus
 
     /** GNSS receiver time fault **/
     GPX_STATUS_FAULT_GNSS_RCVR_TIME                     = (int)0x00100000,
+    /** RTOS task period overrun **/
+    GPX_STATUS_FAULT_RTOS_TASK_PERIOD_OVERRUN           = (int)0x00200000,
     /** DMA Fault detected **/
     GPX_STATUS_FAULT_DMA                                = (int)0x00800000,
 
