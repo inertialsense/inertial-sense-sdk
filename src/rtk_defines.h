e13702342e85cc89254640f919a269c78fc804e8
Alex Bogdanov <alexander.bogdanov@hexagon.com>
2026-05-29 15:58:13 -0700

    max number of observations

--- src/rtk_defines.h
+++ src/rtk_defines.h
@@ -8,7 +8,7 @@
 // #define ENASBS
 
 #define NUMSATSOL   30      // Max number of observations used in the solution
-#define MAXOBS      56      // Max number of observations used in pre-buffer
+#define MAXOBS      55      // Max number of observations used in pre-buffer
 #define NFREQ       2       // Number of carrier frequencies
 #define NEXOBS      0       // Number of extended obs codes
 
