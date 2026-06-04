1c237d0a9f741a8af53e54fe4d38a77503f9d91c
tmcd249 <tmcd249@msn.com>
2026-06-04 10:23:23 -0600

    update sdk

--- src/data_sets.h
+++ src/data_sets.h
@@ -3301,21 +3301,23 @@ enum eIoConfig
     IO_CONFIG_GNSS_SOURCE_LAST                   = IO_CONFIG_GNSS_SOURCE_SER2,    // set to last source
 
     /** GPS type MASK */
-    IO_CONFIG_GNSS_TYPE_MASK                     = (int)0x00000007,
+    IO_CONFIG_GNSS_TYPE_MASK                    = (int)0x00000007,
     /** GPS type - Unused.  USE this when adding a new GNSS Receiver */
-    IO_CONFIG_GNSS_TYPE_NONE                     = (int)0,
+    IO_CONFIG_GNSS_TYPE_NONE                    = (int)0,
     /** GPS type - ublox ZED-F9P w/ RTK */
-    IO_CONFIG_GNSS_TYPE_UBX_F9P                  = (int)1,
+    IO_CONFIG_GNSS_TYPE_UBX_F9P                 = (int)1,
     /** GPS type - NMEA */
-    IO_CONFIG_GNSS_TYPE_NMEA                     = (int)2,
+    IO_CONFIG_GNSS_TYPE_NMEA                    = (int)2,
     /** GPS type - InertialSense GPX */
-    IO_CONFIG_GNSS_TYPE_GPX                      = (int)3,
+    IO_CONFIG_GNSS_TYPE_GPX                     = (int)3,
     /** GPS type - Septentrio */
-    IO_CONFIG_GNSS_TYPE_SEPTENTRIO               = (int)4,
+    IO_CONFIG_GNSS_TYPE_SEPTENTRIO              = (int)4,
     /** GPS type - Host (pass-through from connected IMX host) */
     IO_CONFIG_GNSS_TYPE_ISB                     = (int)5,
+    /** GPS type - ublox X20 w/ RTK */
+    IO_CONFIG_GNSS_TYPE_UBX_X20                 = (int)6,
     /** GPS type - last type */
-    IO_CONFIG_GNSS_TYPE_LAST                     = IO_CONFIG_GNSS_TYPE_ISB,		// Set to last type
+    IO_CONFIG_GNSS_TYPE_LAST                    = IO_CONFIG_GNSS_TYPE_UBX_X20,		// Set to last type
 
 #define IO_CONFIG_GNSS1_SOURCE(ioConfig)     (((ioConfig)>>IO_CONFIG_GNSS1_SOURCE_OFFSET)&IO_CONFIG_GNSS_SOURCE_MASK)
 #define IO_CONFIG_GNSS2_SOURCE(ioConfig)     (((ioConfig)>>IO_CONFIG_GNSS2_SOURCE_OFFSET)&IO_CONFIG_GNSS_SOURCE_MASK)
