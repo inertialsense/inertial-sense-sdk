472088d3267ecf5fe2116b8866271c2436df389e
Alexander Bogdanov <alex.bogdanov@inertialsense.com>
2026-09-11 13:02:38 -0700

    fix for the plot crash (#1306)
    Co-authored-by: Alex Bogdanov <alexander.bogdanov@hexagon.com>

--- python/inertialsense/logInspector/logPlotter.py
+++ python/inertialsense/logInspector/logPlotter.py
@@ -1277,6 +1277,8 @@ class logPlot:
     def angle_unwrap(self, angle):
         unwrap = 0.0
         result = np.empty_like(angle)
+        if np.shape(angle)[0] == 0:
+            return result
         anglePrev = angle[0]
         for i in range(np.shape(angle)[0]):
             result[i] = angle[i] + unwrap
@@ -1428,7 +1430,8 @@ class logPlot:
                         delta = self.vec3_wrap(intEuler - refEuler)
                         sumDelta += delta
                         sumCount += 1
-                refEuler += sumDelta / sumCount
+                if sumDelta is not None:
+                    refEuler += sumDelta / sumCount
 
         for d in self.active_devs:
             qn2b = self.getData(d, DID_INS_2, 'qn2b')
@@ -1508,8 +1511,10 @@ class logPlot:
         self.configureSubplot(ax[2,0], 'INS Heading', 'deg')
 
         refRtkTime = None
+        refRtkHdg = None
         refInsTime = None
-        
+        refEuler = None
+
         if self.residual:
             self.configureSubplot(ax[0,1], 'Heading Residual: Magnetic - INS', 'deg')
             self.configureSubplot(ax[1,1], 'Heading Residual: RTK - Mean', 'deg')
@@ -1523,6 +1528,8 @@ class logPlot:
                 for d in self.active_devs:
                     gnssTime = getTimeFromGpsTowMs(self.getData(d, DID_GNSS2_RTK_CMP_REL, 'timeOfWeekMs'))
                     gnssHdg = self.getData(d, DID_GNSS2_RTK_CMP_REL, 'baseToRoverHeading')
+                    if len(gnssTime) == 0 or len(gnssHdg) == 0:
+                        continue    # No RTK compassing data for this device
                     if refRtkTime is None:
                         refRtkTime = gnssTime
                         refRtkHdg = np.copy(gnssHdg)
@@ -1533,7 +1540,8 @@ class logPlot:
                         delta = self.angle_wrap(intRtkHdg - refRtkHdg)
                         sumDelta += delta
                         sumCount += 1
-                refRtkHdg += sumDelta / sumCount
+                if sumDelta is not None:
+                    refRtkHdg += sumDelta / sumCount
 
             # Reference INS does not exist.  Compute reference from average INS.
             if refInsTime is None:
@@ -1542,7 +1550,7 @@ class logPlot:
                 for d in self.active_devs:
                     time = getTimeFromGpsTow(self.getData(d, DID_INS_2, 'timeOfWeek'))
                     qn2b = self.getData(d, DID_INS_2, 'qn2b')
-                    if len(qn2b) == 0:
+                    if len(time) == 0 or len(qn2b) == 0:
                         continue
                     # Adjust data for attitude bias
                     quat = mul_ConjQuat_Quat(self.log.mount_bias_quat[d,:], qn2b)
@@ -1559,7 +1567,8 @@ class logPlot:
                         delta = self.vec3_wrap(intEuler - refEuler)
                         sumDelta += delta
                         sumCount += 1
-                refEuler += sumDelta / sumCount
+                if sumDelta is not None:
+                    refEuler += sumDelta / sumCount
 
         for d in self.active_devs:
             magTime = getTimeFromGpsTowMs(self.getData(d, DID_INL2_MAG_OBS_INFO, 'timeOfWeekMs'), True)
@@ -1585,7 +1594,7 @@ class logPlot:
                     intMagHdg = np.interp(insTime, magTime, unwrapMagHdg, right=np.nan, left=np.nan)
                     resMagHdg = self.angle_wrap(intMagHdg - insHdg)
                     ax[0,1].plot(insTime, resMagHdg*RAD2DEG)
-                if gnssTime.any():
+                if gnssTime.any() and refRtkHdg is not None:
                     unwrapGnssHdg = self.angle_unwrap(gnssHdg)
                     intInsHdg = np.interp(refRtkTime, gnssTime, unwrapGnssHdg, right=np.nan, left=np.nan)
                     resInsHdg = self.angle_wrap(intInsHdg - refRtkHdg)
