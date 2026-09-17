d6918b6066c636df3ec9ada2779d30523e5fb518
waltjohnson <walt@inertialsense.com>
2026-09-16 18:50:01 -0600

    last minute fix

--- src/pybindMacros.h
+++ src/pybindMacros.h
@@ -66,6 +66,7 @@ PYBIND11_NUMPY_DTYPE(sensors_w_temp_t, imus, temp, mag);
 
 PYBIND11_NUMPY_DTYPE(sys_sensors_adc_t, time, imu, mag, bar, barTemp, humidity, ana);
 PYBIND11_NUMPY_DTYPE(rtos_info_t, freeHeapSize, mallocSize, freeSize, task);
+PYBIND11_NUMPY_DTYPE(is_time_t, week, timeOfWeekMs, status, syncCnt, reserved);
 PYBIND11_NUMPY_DTYPE(inl2_states_t, timeOfWeek, qe2b, ve, ecef, biasPqr, biasAcc, biasBaro, magDec, magInc);
 PYBIND11_NUMPY_DTYPE(inl2_status_t, ahrs, zero_accel, zero_angrate, accel_motion, rot_motion, zero_vel, ahrs_gnss_cnt, hdg_err, hdg_coarse, hdg_aligned, hdg_aligning, ekf_init_done, mag_cal_good, mag_cal_done, stat_magfield);
 PYBIND11_NUMPY_DTYPE(magnetometer_t, time, mag);
@@ -82,6 +83,12 @@ PYBIND11_NUMPY_DTYPE(diag_msg_t, timeOfWeekMs, messageLength, message);
 PYBIND11_NUMPY_DTYPE(survey_in_t, state, maxDurationSec, minAccuracy, elapsedTimeSec, hAccuracy, lla);
 PYBIND11_NUMPY_DTYPE(port_monitor_set_t, portInfo, status, txBytesPerSec, rxBytesPerSec, txBytes, rxBytes, txDataDrops, rxOverflows, txBytesDropped, rxChecksumErrors);
 PYBIND11_NUMPY_DTYPE(port_monitor_t, port, activePorts);
+PYBIND11_NUMPY_DTYPE(ext_aiding_pos_t, timeOfWeekMs, status, pos, offset, var);
+PYBIND11_NUMPY_DTYPE(ext_aiding_vel_t, timeOfWeekMs, status, vel, offset, var);
+PYBIND11_NUMPY_DTYPE(ext_aiding_speed_t, timeOfWeekMs, status, speed, var, offset);
+PYBIND11_NUMPY_DTYPE(ext_aiding_dir_speed_t, timeOfWeekMs, status, speed, var, offset, direction);
+PYBIND11_NUMPY_DTYPE(ext_aiding_heading_t, timeOfWeekMs, status, heading, var);
+PYBIND11_NUMPY_DTYPE(ext_aiding_attitude_t, timeOfWeekMs, status, att, var);
 
 // PYBIND11_NUMPY_DTYPE(evb2_t, week, timeOfWeekMs, firmwareVer, comBridgeCfg, loggerMode, loggerElapsedTimeMs, wifiSSID, wifiPSK, wifiIpAddr, serverIpAddr, serverPort);
 // PYBIND11_NUMPY_DTYPE(evb_status_t, week, timeOfWeekMs, firmwareVer, evbStatus, loggerMode, loggerElapsedTimeMs, wifiIpAddr, sysCommand);
